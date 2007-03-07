61e0716bea42cfd995842fe94b4fc24649780b53
shnavid <shnavid@ef72aa8b-4018-0410-8976-d6e080ef94d8>
2007-03-07 15:44:07 +0000

    Added further commands' output to gather, taken from Alisdar's own LVM troubleshooting script (lvm_dump):
    http://sources.redhat.com/cgi-bin/cvsweb.cgi/~checkout~/LVM2/scripts/lvm_dump.sh?cvsroot=lvm2
    
    
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/sos/trunk@91 ef72aa8b-4018-0410-8976-d6e080ef94d8

--- src/lib/sos/plugins/devicemapper.py
+++ src/lib/sos/plugins/devicemapper.py
@@ -18,11 +18,18 @@ class devicemapper(sos.plugintools.PluginBase):
     """This plugin gathers device-mapper related information (dm, lvm, multipath)
     """
     def setup(self):
-        self.collectExtOutput("/sbin/dmsetup info")
+        self.collectExtOutput("/sbin/dmsetup info -c")
         self.collectExtOutput("/sbin/dmsetup table")
+        self.collectExtOutput("/sbin/dmsetup status")
+
+        self.collectExtOutput("/usr/sbin/vgscan -vvv")
+        self.collectExtOutput("/usr/sbin/vgdisplay -vv")
+        self.collectExtOutput("/usr/sbin/pvscan -v")
+        self.collectExtOutput("/usr/sbin/lvs -a -o +devices")
+        self.collectExtOutput("/usr/sbin/pvs -a -v")
+        self.collectExtOutput("/usr/sbin/vgs -v")
 
         self.addCopySpec("/etc/lvm/lvm.conf")
-        self.collectExtOutput("/usr/sbin/vgdisplay -vv")
 
         self.addCopySpec("/etc/multipath.conf")
         self.addCopySpec("/var/lib/multipath/bindings")
