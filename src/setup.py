02fe8bdb344b00ee8116f8fef9580155de45eafc
shnavid <shnavid@ef72aa8b-4018-0410-8976-d6e080ef94d8>
2007-07-13 13:26:36 +0000

    * stripped unnecessary bits out of setup.py
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/sos/trunk@202 ef72aa8b-4018-0410-8976-d6e080ef94d8

--- src/setup.py
+++ src/setup.py
@@ -3,25 +3,11 @@ setup.py - Setup package with the help from Python's DistUtils
 """
 
 from distutils.core import setup
-from ConfigParser import ConfigParser
-import sys,os,time
 
-# change version in spec file along with this string
 setup(
 	name = 'sos',
-	version = '1.7',
-	license = 'GPL',
-	description = 'System Support Tools',
-	long_description = """Sos is a set of tools that gathers information about system
-hardware and configuration. The information can then be used for
-diagnostic purposes and debugging. Sos is commonly used to help
-support technicians and developers.""",
-	author = 'Steve Conklin, et al.',
-	author_email = 'sconklin@redhat.com',
-	url = 'http://sos.108.redhat.com/',
 	packages = ['sos', 'sos.plugins'],
 	scripts = [],
 	package_dir = {'': 'lib',},
-	# data_files is broken for building dists, works for installs
 	data_files = [('/usr/sbin', ['sosreport']), ('/usr/share/man/man1', ['sosreport.1']) ]
 )
