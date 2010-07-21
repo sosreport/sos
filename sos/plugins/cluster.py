30737845dc3168f7509e47fe3b35cf3597d68caf
astokes <astokes@ef72aa8b-4018-0410-8976-d6e080ef94d8>
2010-07-21 18:59:44 +0000

    update cluster packages
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/sos/trunk@977 ef72aa8b-4018-0410-8976-d6e080ef94d8

--- sos/plugins/cluster.py
+++ sos/plugins/cluster.py
@@ -34,8 +34,8 @@ class cluster(sos.plugintools.PluginBase):
                               "gfs-utils", "gnbd", "kmod-gfs", "kmod-gnbd", "lvm2-cluster", "gfs2-utils" ]
 
         elif rhelver == 6:
-            self.packages = [ "ricci", "corosync", "fenced",
-                              "dlm_controld", "gfs_controld" ]
+            self.packages = [ "ricci", "corosync", "openais",
+                              "cman", "clusterlib", "fence-agents" ]
 
         self.files = [ "/etc/cluster/cluster.conf" ]
         return sos.plugintools.PluginBase.checkenabled(self)
