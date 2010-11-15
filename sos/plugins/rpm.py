aefaa6d1d0b2259d8def7ddde5b19e1740dd1199
pcarrier <pcarrier@ef72aa8b-4018-0410-8976-d6e080ef94d8>
2010-11-15 22:55:50 +0000

    More extensive rpm info (pure INSTALLTIME, VENDOR)
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/sos/trunk@1003 ef72aa8b-4018-0410-8976-d6e080ef94d8

--- sos/plugins/rpm.py
+++ sos/plugins/rpm.py
@@ -24,7 +24,7 @@ class rpm(sos.plugintools.PluginBase):
         self.addCopySpec("/var/log/rpmpkgs")
 
         if self.getOption("rpmq"):
-            self.collectExtOutput("/bin/rpm -qa --qf=\"%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}~~%{INSTALLTIME:date}\n\" --nosignature --nodigest|/bin/awk -F ~~ '{printf \"%-60s%s\\n\",$1,$2}'|sort", root_symlink = "installed-rpms")
+            self.collectExtOutput("/bin/rpm -qa --qf=\"%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}~~%{INSTALLTIME:date}\t%{INSTALLTIME}\t%{VENDOR}\n\" --nosignature --nodigest|/bin/awk -F ~~ '{printf \"%-60s%s\\n\",$1,$2}'|sort", root_symlink = "installed-rpms")
 
         if self.getOption("rpmva"):
             self.collectExtOutput("/bin/rpm -Va", root_symlink = "rpm-Va", timeout = 3600)
