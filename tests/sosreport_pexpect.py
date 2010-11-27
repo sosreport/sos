c4d03d9b99ee4f8c464b46c90101510681bc5c0e
pcarrier <pcarrier@ef72aa8b-4018-0410-8976-d6e080ef94d8>
2010-11-27 23:33:41 +0000

    [tests] Replaced old expect scripts with pexpect
    git-svn-id: svn+ssh://svn.fedorahosted.org/svn/sos/trunk@1048 ef72aa8b-4018-0410-8976-d6e080ef94d8

---
+++ tests/sosreport_pexpect.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import unittest
+import pexpect
+
+from re import search, escape
+from os import kill
+from signal import SIGINT
+
+class PexpectTest(unittest.TestCase):
+    def test_plugins_install(self):
+        sos = pexpect.spawn('/usr/sbin/sosreport -l')
+        try:
+            sos.expect('plugin.*does not install, skipping')
+        except pexpect.EOF:
+            pass
+        else:
+            self.fail("a plugin does not install or sosreport is too slow")
+        kill(sos.pid, SIGINT)
+
+    def test_batchmode_removes_questions(self):
+        sos = pexpect.spawn('/usr/sbin/sosreport --batch')
+        grp = sos.expect('send this file to your support representative.', 15)
+        self.assertEquals(grp, 0)
+        kill(sos.pid, SIGINT)
+
+if __name__ == '__main__':
+    unittest.main()
