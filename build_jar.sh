47d132751f202850ce294c078c6fdeca1fd2bf39
Jesse Jaggars <jjaggars@redhat.com>
2011-07-22 14:09:00 -0500

    adding a script to build a jar for jython

---
+++ build_jar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+JYTHON_STANDALONE_JAR=$1
+
+BUILD_DIR=buildjar
+DEST_JAR=$BUILD_DIR/sosreport.jar
+PO_DIR=$BUILD_DIR/sos/po
+
+cp $JYTHON_STANDALONE_JAR $DEST_JAR
+
+echo "Making build directories ..."
+mkdir -p $PO_DIR
+
+for po in `ls po/*.po`;
+do
+    dest=`basename $po | awk -F. '{print $1}'`
+    dest=${PO_DIR}/${dest}.properties
+
+    echo "Converting $po to $dest ..."
+    msgcat -p -o $dest $po
+done
+
+echo "Duplicating en ..."
+cp $PO_DIR/en.properties $PO_DIR/en_US.properties
+
+echo "Pulling in standalone jython.jar ..."
+cp $JYTHON_STANDALONE_JAR $DEST_JAR
+
+echo "Adding in sos ..."
+zip -r $DEST_JAR sos
+
+echo "Adding in __run__.py ..."
+zip -r $DEST_JAR __run__.py
+
+echo "Adding in i18n ..."
+cd $BUILD_DIR
+zip -r sosreport.jar sos
+
+echo "Cleaning up ..."
+cd -
+rm -rf $BUILD_DIR/sos
