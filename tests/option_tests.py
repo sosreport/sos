80e99a2d290b10ed908320c97ca11874aac422b0
Jesse Jaggars <jjaggars@redhat.com>
2012-02-14 09:27:36 -0600

    adding root_symlink support under tarfiles and fixing getOption behavior

--- tests/option_tests.py
+++ tests/option_tests.py
@@ -10,15 +10,25 @@ class GlobalOptionTest(unittest.TestCase):
         self.commons = {
             'global_plugin_options': {
                 'test_option': 'foobar',
+                'baz': None,
+                'empty_global': True,
             },
         }
         self.plugin = Plugin(self.commons)
+        self.plugin.optNames = ['baz', 'empty']
+        self.plugin.optParms = [{'enabled': False}, {'enabled': None}]
 
     def test_simple_lookup(self):
         self.assertEquals(self.plugin.getOption('test_option'), 'foobar')
 
     def test_multi_lookup(self):
         self.assertEquals(self.plugin.getOption(('not_there', 'test_option')), 'foobar')
+
+    def test_cascade(self):
+        self.assertEquals(self.plugin.getOption(('baz')), False)
+
+    def test_none_should_cascade(self):
+        self.assertEquals(self.plugin.getOption(('empty', 'empty_global')), True)
 
 if __name__ == "__main__":
     unittest.main()
