120a92e1378803397ad14d0cbf3cbbb1a67a5af5
Adam Stokes <adam.stokes@canonical.com>
2012-02-23 22:45:59 +0000

    sos/plugins: 2 new Ubuntu specific and ported plugins
    2 New plugins that were relevant to Ubuntu and other Debian
    based systems. Altered a few more base plugins to handle
    Ubuntu platforms as well.
    
    Signed-off-by: Adam Stokes <adam.stokes@canonical.com>

--- sos/plugins/hardware.py
+++ sos/plugins/hardware.py
@@ -12,10 +12,10 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-from sos.plugins import Plugin, RedHatPlugin
+from sos.plugins import Plugin, RedHatPlugin, UbuntuPlugin
 from glob import glob
 
-class hardware(Plugin, RedHatPlugin):
+class hardware(Plugin, RedHatPlugin, UbuntuPlugin):
     """hardware related information
     """
     def setup(self):
