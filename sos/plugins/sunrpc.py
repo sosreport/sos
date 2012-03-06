392fce9fa0630ee6d3153d16f1e257de0dd0cd25
Bryn M. Reeves <bmr@redhat.com>
2012-03-06 10:20:00 +0000

    Add new sunrpc module to collect rpcinfo for gluster systems
    Resolves: bz784061

---
+++ sos/plugins/sunrpc.py
@@ -0,0 +1,30 @@
+## Copyright (C) 2012 Red Hat, Inc., Bryn M. Reeves <bmr@redhat.com>
+
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import sos.plugintools
+
+class sunrpc(sos.plugintools.PluginBase):
+    """Sun RPC related information
+    """
+    def checkenabled(self):
+       if self.policy().runlevelDefault() in self.policy().runlevelByService("rpcbind"):
+          return True
+       return False
+
+    def setup(self):
+        self.collectExtOutput("/usr/sbin/rpcinfo -p localhost")
+        return
+
