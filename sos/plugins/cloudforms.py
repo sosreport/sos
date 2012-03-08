b84cfa4a83b03ea5385cc3b0149eaf6c7602774c
Bryn M. Reeves <bmr@redhat.com>
2012-03-08 09:10:00 +0000

    Correct aeolus debug invocation in CloudForms module
    Resolves: bz752666

--- sos/plugins/cloudforms.py
+++ sos/plugins/cloudforms.py
@@ -1,20 +1,5 @@
-## Copyright (C) 2012 Red Hat Inc., Bryn M. Reeves <bmr@redhat.com>
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
 import sos.plugintools
-import os.path, os.mkdir
+import os
 
 class cloudforms(sos.plugintools.PluginBase):
     """CloudForms related information
@@ -24,19 +9,19 @@ class cloudforms(sos.plugintools.PluginBase):
         return False
 
     def checkenabled(self):
-        # enable if any related package is installed
-        self.packages = ["katello", "katello-common", \
-                    "katello-headpin", "aeoleus-conductor" ]
-
+        self.packages = ["katello", "katello-common",
+                        "katello-headpin", "aeoleus-conductor"]
+        self.files = ["/usr/share/katello/script/katello-debug",
+                        "/usr/bin/aeolus-debug"]
         return sos.plugintools.PluginBase.checkenabled(self)
 
     def setup(self):
         katello_debug = "/usr/share/katello/script/katello-debug"
         aeolus_debug = "/usr/bin/aeolus-debug"
-
         if os.path.isfile(katello_debug):
             katello_debug_path = os.path.join(self.cInfo['dstroot'],"katello-debug")
             self.collectExtOutput("%s -d %s" % katello_debug, katello_debug_path)
         if os.path.isfile(aeolus_debug):
-            aeolus_debug_path = os.path.join(self.cInfo['dstroot'],"aeolus-debug"))
+            aeolus_debug_path = os.path.join(self.cInfo['dstroot'],"aeolus-debug")
             self.collectExtOutput("%s -d %s" % katello_debug, aeolus_debug_path)
+
