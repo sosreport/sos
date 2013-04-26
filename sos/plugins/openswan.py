44948f8a3eb6bc3430698a4e8ecb541fc941cfa6
Bryn M. Reeves <bmr@redhat.com>
2013-04-26 17:42:33 +0100

    Enable Openswan plug-in on more distributions
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/openswan.py
+++ sos/plugins/openswan.py
@@ -14,9 +14,9 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-from sos.plugins import Plugin, RedHatPlugin
+from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
 
-class Openswan(Plugin, RedHatPlugin):
+class Openswan(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
     """ipsec related information
     """
 
