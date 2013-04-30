071e2348d1d54216849f2be9d0a1ee244ecc126d
Bryn M. Reeves <bmr@redhat.com>
2013-04-30 16:57:22 +0100

    Add new SCSI plug-in
    Separate out the scsi data from the hardware plug-in.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/scsi.py
@@ -0,0 +1,37 @@
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin
+from glob import glob
+import os
+
+class Scsi(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
+    """hardware related information
+    """
+
+    plugin_name = 'scsi'
+
+    def setup(self):
+        self.add_copy_specs([
+            "/proc/scsi",
+            "/etc/stinit.def",
+            "/sys/bus/scsi",
+            "/sys/class/scsi_host",
+            "/sys/class/scsi_disk",
+            "/sys/class/scsi_device",
+            "/sys/class/scsi_generic"
+        ])
+        
+        self.add_cmd_output("lsscsi")
+        self.add_cmd_output("sg_map")
