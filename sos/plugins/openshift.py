252956bff9f04af50078f0e002e1fe2e25bc2530
Nick Harvey <nickharveyonline@gmail.com>
2013-05-29 11:31:34 -0400

    created a plugin for Openshift

---
+++ sos/plugins/openshift.py
@@ -0,0 +1,40 @@
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin
+
+class Openshift(Plugin, RedHatPlugin):
+    '''Openshift related information'''
+
+    plugin_name = 'openshift'
+
+    option_list = [("broker", 'Gathers broker specific files', 'fast', 0),
+		   ("node", 'Gathers node specific files', 'fast', 0)]
+
+    def setup(self):
+	    self.add_copy_spec("/etc/openshift-enterprise-version")
+
+	    if self.option_enabled("broker"):
+		    self.add_copy_spec("/var/log/activemq")
+		    self.add_copy_spec("/var/log/mongodb")
+		    self.add_copy_spec("/var/log/openshift")
+		    self.add_copy_spec("/var/www/openshift")
+		    self.add_copy_spec("/etc/openshift/")
+		    
+		    self.collectExtOuput("bin/oo-accpet-broker -v")
+
+	    if self.option_enabled("node"):
+		    self.add_copy_spec("/var/log/openshift/node")
+		    
+		    self.collectExtOuput("bin/oo-accept-node -v")
