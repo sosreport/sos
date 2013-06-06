3abe8e91795f5774f03b178fb23c4d3d843ad25c
Bryn M. Reeves <bmr@redhat.com>
2013-06-06 15:43:06 +0100

    Fix typo in logs plug-in
    s/)[/])/
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/logs.py
+++ sos/plugins/logs.py
@@ -32,7 +32,7 @@ class Logs(Plugin):
         self.add_copy_specs([
             "/etc/syslog.conf",
             "/etc/rsyslog.conf"
-        )]
+        ])
 
         self.limit = self.get_option("syslogsize")
         self.add_copy_spec_limit("/var/log/boot*", sizelimit = self.limit)
