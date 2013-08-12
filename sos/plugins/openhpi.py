90531cc000690e30a74df025e97dd39950da6fae
Bryn M. Reeves <bmr@redhat.com>
2013-08-12 20:11:29 +0100

    Add new plug-in to gather OpenHPI configuration
    New plug-in to collect OpenHPI configuration files. These files
    may contain passwords; a regex substitution rule has been added
    based on information from the reporter but this needs testing
    with a range of actual configuration files.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/openhpi.py
@@ -0,0 +1,34 @@
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin
+import os
+
+class OpenHPI(Plugin, RedHatPlugin):
+    """OpenHPI related information
+    """
+
+    plugin_name = 'openhpi'
+
+    def setup(self):
+        self.add_copy_specs([
+            "/etc/openhpi/openhpi.conf",
+            "/etc/openhpi/openhpiclient.conf"
+        ])
+
+    def postproc(self):
+        self.do_file_sub("/etc/openhpi/openhpi.conf"
+                        r'([Pp]assw(or)?d|[Pp]assphrase)[[:space:]]+\=[[:space:]]"(.*)"',
+                        r"\1******")
+
