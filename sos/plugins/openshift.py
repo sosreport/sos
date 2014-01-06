b8dd5356905c8459ee2675c149594a88540b1f83
Eric Rich <erich@redhat.com>
2014-01-06 16:17:50 -0500

    Adding 2.0 files and commands.

--- sos/plugins/openshift.py
+++ sos/plugins/openshift.py
@@ -17,7 +17,7 @@ from sos.plugins import Plugin, RedHatPlugin
 class Openshift(Plugin, RedHatPlugin):
     '''Openshift related information'''
 
-    plugin_name = "Openshift"
+    plugin_name = "openshift"
 
     option_list = [("broker", "Gathers broker specific files", "slow", False),
 		   ("node", "Gathers node specific files", "slow", False)]
@@ -25,34 +25,38 @@ class Openshift(Plugin, RedHatPlugin):
     def setup(self):
 	    self.add_copy_specs(["/etc/openshift-enterprise-version",
 		    "/etc/openshift/",
+                    "/var/log/openshift/",
 		    "/etc/dhcp/dhclient-*"])
 
-	    if self.option_enabled("broker"):
-		    self.add_copy_specs(["/var/log/activemq",
-				    "/var/log/mongodb",
-				    "/var/log/openshift",
-				    "/var/www/openshift/broker/log",
-				    "/var/www/openshift/broker/httpd/logs/",
-				    "/var/www/openshift/console/log",
-				    "/var/www/openshift/console/httpd/logs",
-				    "/var/log/openshift/user_action.log"])
+            self.add_cmd_output("oo-diagnostics")
 
-		    self.add_cmd_output("oo-accpet-broker -v")
+	    if self.option_enabled("broker"):
+		    self.add_copy_specs(["/var/log/activemq/",
+				    "/var/log/mongodb/",
+        			    "/var/log/mcollective-client.log",
+        			    "/var/log/ruby193-mcollective-client.log",
+				    "/var/log/openshift/broker/",
+				    "/var/log/openshift/console/"])
+
+		    self.add_cmd_output("oo-accept-broker -v")
 		    self.add_cmd_output("oo-admin-chk -v")
 		    self.add_cmd_output("mco ping")
+		    self.add_cmd_output("oo-mco ping")
 		    self.add_cmd_output("gem list --local")
-		    self.add_cmd_output("cd /var/www/openshift/broker/ && bundle --local")
+		    self.add_cmd_output("scl enable ruby193 'gem list --local'")
+		    #self.add_cmd_output("cd /var/www/openshift/broker/ && scl enable ruby193 'bundle --local'")
 
 	    if self.option_enabled("node"):
-		    self.add_copy_specs(["/var/log/openshift/node",
-			    "/cgroup/all/openshift",
-			    "/var/log/mcollective.log",
-			    "/var/log/openshift-gears-async-start.log",
-			    "/var/log/httpd/error_log"])
+		    self.add_copy_specs(["/var/log/openshift/node/",
+        			    "/cgroup/*/openshift/",
+        			    "/var/log/mcollective.log",
+        			    "/opt/rh/ruby193/root/etc/mcollective/",
+        			    "/var/log/openshift-gears-async-start.log",
+        			    "/var/log/httpd/"])
 
 		    self.add_cmd_output("oo-accept-node -v")
 		    self.add_cmd_output("oo-admin-ctl-gears list")
-		    self.add_cmd_output("ls -l /var/lib/openshift")
+		    self.add_cmd_output("ls -l /var/lib/openshift/")
 
     def postproc(self):
 	    self.do_file_sub('/etc/openshift/broker.conf',
