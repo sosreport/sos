23cdbd97a6bd2e44d87f01d6bf90b66667c4d7be
Bryn M. Reeves <bmr@redhat.com>
2014-02-03 12:09:57 +0000

    Fix verbose file logging
    Prior versions of sos enable debug logging to the embedded log
    file (sos_logs/sos.log) when a single '-v' is given. Restore this
    behaviour and ensure that command-not-found messages are reported
    at 'info' rather than 'warning' level.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/sosreport.py
+++ sos/sosreport.py
@@ -651,6 +651,7 @@ class SoSReport(object):
                 flog.setLevel(logging.DEBUG)
             elif self.opts.verbosity > 0:
                 console.setLevel(logging.INFO)
+                flog.setLevel(logging.DEBUG)
             else:
                 console.setLevel(logging.WARNING)
             self.soslog.addHandler(console)
