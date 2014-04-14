2ba850f752a68f5c18393230d4df7f2f713f3e70
Bryn M. Reeves <bmr@redhat.com>
2014-04-14 19:44:07 +0100

    Make vendor and ruby version class variables in OpenShift plugin
    Allow the vendor path and ruby version to be overridden by
    subclasses without needing to duplicate code.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/openshift.py
+++ sos/plugins/openshift.py
@@ -22,6 +22,9 @@ class Openshift(Plugin, RedHatPlugin):
     option_list = [("broker", "Gathers broker specific files", "slow", False),
            ("node", "Gathers node specific files", "slow", False)]
 
+    ruby = "ruby193"
+    vendor ="rh"
+
     def setup(self):
         self.add_copy_specs([
             "/etc/openshift-enterprise-version",
@@ -55,7 +58,7 @@ class Openshift(Plugin, RedHatPlugin):
                 "/var/log/openshift/node",
                 "/cgroup/*/openshift/",
                 "/var/log/mcollective.log",
-                "/opt/rh/ruby193/root/etc/mcollective/",
+                "/opt/%s/%s/root/etc/mcollective/" % (self.vendor, self.ruby),
                 "/var/log/openshift-gears-async-start.log",
             ])
 
