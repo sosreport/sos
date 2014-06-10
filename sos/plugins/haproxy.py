9803fd1174ce6c2dff558504e9b53f1ad2c67abb
Bryn M. Reeves <bmr@redhat.com>
2014-06-10 21:47:51 +0100

    [haproxy] new plugin
    Add a plugin for HAProxy, based on a suggestion from Brandon
    Perkins in rhbz#1107865.
    
    Backported to 2.2.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/haproxy.py
@@ -0,0 +1,28 @@
+## Copyright (C) 2014 Red Hat, Inc. Bryn M. Reeves <bmr@redhat.com>
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import sos.plugintools
+
+class haproxy(sos.plugintools.PluginBase):
+    """haproxy information
+    """
+
+    packages = ('haproxy',)
+
+    def setup(self):
+        self.addCopySpec("/etc/haproxy/haproxy.cfg")
+        self.collectExtOutput("haproxy -f /etc/haproxy/haproxy.cfg -c")
+
+# vim: et ts=4 sw=4
