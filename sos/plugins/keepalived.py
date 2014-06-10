e65c789344951139b9c741c34c71af746c465f13
Bryn M. Reeves <bmr@redhat.com>
2014-06-10 20:54:09 +0100

    [keepalived] new plugin
    Fixes #302.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/keepalived.py
@@ -0,0 +1,33 @@
+## Copyright (C) 2014 Red Hat, Inc. Jamie Bainbridge <jbainbri@redhat.com>
+## Copyright (C) 2014 Red Hat, Inc. Bryn M. Reeves <bmr@redhat.com>
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin
+
+class Keepalived(Plugin, RedHatPlugin):
+    """Keepalived information
+    """
+
+    plugin_name = 'keepalived'
+
+    packages = ('keepalived',)
+
+    def setup(self):
+        self.add_copy_specs([
+            "/etc/keepalived/keepalived.conf",
+            "/etc/sysconfig/keepalived"
+        ])
+
+# vim: et ts=4 sw=4
