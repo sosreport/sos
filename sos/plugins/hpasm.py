1b49864a0c764f8e853c8cc9def0626d2e52f24b
Bryn M. Reeves <bmr@redhat.com>
2014-06-13 17:14:38 +0100

    [hpasm] new plugin
    Collect information for hp Server Management Drivers and Agent.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/hpasm.py
@@ -0,0 +1,28 @@
+### This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import sos.plugintools
+from os.path import exists
+
+class hpasm(sos.plugintools.PluginBase):
+    """HP ASM (hp Server Management Drivers and Agent) information
+    """
+
+    packages = ('hp-health',)
+ 
+    def setup(self):
+        self.addCopySpec("/var/log/hp-health/hpasmd.log")
+        self.collectExtOutput("hpasmcli -s 'show asr'")
+        self.collectExtOutput("hpasmcli -s 'show server'")
+
