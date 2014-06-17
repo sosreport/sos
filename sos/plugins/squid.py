sosreport/sos

commit acd4988f630f5f6f1d7c8dc1a9a84c7a7a3005a5

Do not pass directory paths to addCopySpecLimit in squid plug-in
The squid plug-in attempts to collect a directory path with size
limiting via addCopySpecLimit. This does not work with the current
interface.

Replace with separate calls to addCopySpecLimit for each default
log file in the /var/log/squid directory.

Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/plugins/squid.py
@@ -28,7 +28,13 @@ class squid(sos.plugintools.PluginBase):
 
     def setup(self):
         self.addCopySpec("/etc/squid/squid.conf")
-        self.addCopySpecLimit("/var/log/sqid",
-                            sizelimit = self.getOption("logsize"))
+        logsize = self.getOption("logsize")
+                              sizelimit = logsize)  
+        self.addCopySpecLimit("/var/log/sqid/access.log",
+                              sizelimit = logsize)  
+        self.addCopySpecLimit("/var/log/sqid/cache.log",
+                              sizelimit = logsize)  
+        self.addCopySpecLimit("/var/log/sqid/squid.out",
+                              sizelimit = logsize)  
         return
 

commit c4acd5907a3704a49574dc0a30f53fd414e09147

Fix typo in squid plugin
Commit acd4988 introduced a stray line into the squid plugin.
Remove it.

sos/plugins/squid.py
@@ -29,7 +29,6 @@ class squid(sos.plugintools.PluginBase):
     def setup(self):
         self.addCopySpec("/etc/squid/squid.conf")
         logsize = self.getOption("logsize")
-                              sizelimit = logsize)  
         self.addCopySpecLimit("/var/log/sqid/access.log",
                               sizelimit = logsize)  
         self.addCopySpecLimit("/var/log/sqid/cache.log",