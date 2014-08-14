6437a9a2ba1b73df486b38be13c9e2401d719ee8
Jorge Niedbalski R <niedbalski@gmail.com>
2014-08-14 14:42:16 -0400

    [juju] Adds 'juju get' output, exposes export-mongodb
    This commit extends the plugin to include the resulting 'juju get
    ${service}' for each deployed service, also it exposes the
    export-mongodb option, which includes on the report a json file
    containing each data collection used by juju-core.
    
    Signed-off-by: Jorge Niedbalski R <niedbalski@gmail.com>
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- sos/plugins/juju.py
+++ sos/plugins/juju.py
@@ -15,6 +15,25 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 from sos.plugins import Plugin, UbuntuPlugin
+from json import loads as json_load
+
+
+def ensure_service_is_running(service):
+    def wrapper(callback):
+        def wrapped_f(self, *args, **kwargs):
+            try:
+                result = self.call_ext_prog("service {0} stop".format(service))
+                if result["status"] != 0:
+                    raise Exception("Cannot stop {0} service".format(service))
+                callback(self, *args, **kwargs)
+            except Exception as ex:
+                self._log_error("Cannot stop {0}, exception: {1}".format(
+                    service,
+                    ex.message))
+            finally:
+                self.call_ext_prog("service {0} start".format(service))
+        return wrapped_f
+    return wrapper
 
 
 class Juju(Plugin, UbuntuPlugin):
@@ -22,6 +41,33 @@ class Juju(Plugin, UbuntuPlugin):
     """
 
     plugin_name = 'juju'
+
+    option_list = [
+        ('export-mongodb',
+         'Export mongodb collections as json files', '', False),
+    ]
+
+    def get_deployed_services(self):
+        cmd = "juju status --format json"
+        return json_load(
+            self.call_ext_prog(cmd)['output'])['services'].keys()
+
+    @ensure_service_is_running("juju-db")
+    def export_mongodb(self):
+        collections = (
+            "relations",
+            "environments",
+            "linkednetworks",
+            "system",
+            "settings",
+        )
+
+        for collection in collections:
+            self.add_cmd_output(
+                "/usr/lib/juju/bin/mongoexport --ssl \
+                --dbpath=/var/lib/juju/db --db juju --collection {0} \
+                --jsonArray".format(collection),
+                suggest_filename="{}.json".format(collection))
 
     def setup(self):
         self.add_copy_specs([
@@ -33,5 +79,12 @@ class Juju(Plugin, UbuntuPlugin):
             "juju -v status",
             "juju -v get-constraints"
         ])
+
+        for service in self.get_deployed_services():
+            self.add_cmd_output("juju get {}".format(service))
+
+        if self.get_option("export-mongodb"):
+            self.export_mongodb()
+
 
 # vim: et ts=4 sw=4
