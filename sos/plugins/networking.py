64a892a6fc1013246da1cd6f41005ab9d44132fa
Bryn M. Reeves <bmr@redhat.com>
2014-08-21 22:15:23 +0100

    [networking] additional nmcli commands
    Collect 'nmcli con show active' and 'nmcli dev show'.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/networking.py
+++ sos/plugins/networking.py
@@ -105,7 +105,9 @@ class Networking(Plugin):
             "ip neigh show",
             "nmcli general status",
             "nmcli connection show",
+            "nmcli connection show active",
             "nmcli device status",
+            "nmcli device show",
             "biosdevname -d"
         ])
         ip_link_result = self.call_ext_prog("ip -o link")
