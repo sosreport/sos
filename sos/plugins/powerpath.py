45954d982ea447f4cff74e88f8e50b53e8848c0d
Bryn M. Reeves <bmr@redhat.com>
2014-09-18 21:37:43 +0100

    [emc,powerpath] move PowerPath collection into its own plugin
    Move PowerPath functionality into a new powerpath plugin and
    remove the (now empty) EMC plugin.
    
    Fixes: #257.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/powerpath.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2008 EMC Corporation. Keith Kearnan <kearnan_keith@emc.com>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin, os
+
+
+class PowerPath(Plugin, RedHatPlugin):
+    """ EMC PowerPath
+    """
+
+    plugin_name = 'powerpath'
+    profiles = ('storage', 'hardware')
+    packages = ('EMCpower',)
+
+    def get_pp_files(self):
+        """ EMC PowerPath specific information - files
+        """
+        self.add_cmd_output("powermt version")
+        self.add_copy_specs([
+            "/etc/init.d/PowerPath",
+            "/etc/powermt.custom",
+            "/etc/emcp_registration",
+            "/etc/emc/mpaa.excluded",
+            "/etc/emc/mpaa.lams",
+            "/etc/emcp_devicesDB.dat",
+            "/etc/emcp_devicesDB.idx",
+            "/etc/emc/powerkmd.custom",
+            "/etc/modprobe.conf.pp"
+        ])
+
+    def get_pp_config(self):
+        """ EMC PowerPath specific information - commands
+        """
+        self.add_cmd_outputs([
+            "powermt display",
+            "powermt display dev=all",
+            "powermt check_registration",
+            "powermt display options",
+            "powermt display ports",
+            "powermt display paths",
+            "powermt dump"
+        ])
+
+    def setup(self):
+        self.get_pp_files()
+        # If PowerPath is running collect additional PowerPath specific
+        # information
+        if os.path.isdir("/proc/emcp"):
+            self.get_pp_config()
+
+# vim: et ts=4 sw=4
