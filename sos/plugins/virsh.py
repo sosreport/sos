0fe3841d7ca07b342e07f994bf3c3bce8eae7c1c
Pavel Moravec <pmoravec@redhat.com>
2015-05-15 16:10:41 +0200

    [virsh] gather virt-manager logs and some domains info
    Call virsh dumpxml to bypass authentication and limit to 180s.
    
    Resolves #544
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/plugins/virsh.py
+++ sos/plugins/virsh.py
@@ -41,6 +41,7 @@ class LibvirtClient(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
         if domains_file:
             domains_lines = open(domains_file, "r").read().splitlines()[2:]
             for domain in filter(lambda x: x, domains_lines):
-                self.add_cmd_output("virsh dumpxml %s" % domain.split()[1])
+                self.add_cmd_output("virsh -r dumpxml %s" % domain.split()[1],
+                                    timeout=180)
 
 # vim: et ts=4 sw=4
