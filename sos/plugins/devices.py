583c5e33553c7d4af50e2c276920d5ad36821ab8
Pavel Moravec <pmoravec@redhat.com>
2015-05-31 16:24:23 +0200

    [devices] New plugin to collect "udevadm info --export-db"
    Partially resolves #570
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

---
+++ sos/plugins/devices.py
@@ -0,0 +1,28 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
+
+
+class Devices(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
+    """ devices specific commands
+    """
+
+    plugin_name = 'devices'
+    profiles = ('system', 'hardware', 'boot')
+
+    def setup(self):
+        self.add_cmd_output("udevadm info --export-db")
+
+# vim: et ts=4 sw=4
