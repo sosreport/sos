e9b0d2e9d5b8e5987c1ad428559b21003e2a61d1
Luca Miccini <luca.miccini@redhat.com>
2015-06-30 17:54:37 +0200

    [saphana] add path checks vs. /hana/shared and check if python_support is a valid path

--- sos/plugins/saphana.py
+++ sos/plugins/saphana.py
@@ -28,50 +28,55 @@ class saphana(Plugin, RedHatPlugin):
 
         sids = []
 
-        s = os.listdir("/hana/shared")
-        for sid in s:
-            if len(sid) == 3:
-                sid = sid.strip()
-                sids.append(sid)
+        if os.path.isdir("/hana/shared"):
+            s = os.listdir("/hana/shared")
+            for sid in s:
+                if len(sid) == 3:
+                    sid = sid.strip()
+                    sids.append(sid)
 
-        for sid in sids:
-            sidadm = '%sadm' % sid.lower()
+            for sid in sids:
+                sidadm = '%sadm' % sid.lower()
 
-            prefix = 'su - %s -c' % sidadm
+                prefix = 'su - %s -c' % sidadm
 
-            self.add_cmd_output('%s "HDB info"' % prefix,
-                                suggest_filename="%s_HDB_info" % sid)
+                self.add_cmd_output('%s "HDB info"' % prefix,
+                                    suggest_filename="%s_HDB_info" % sid)
 
-            self.add_cmd_output('%s "hdbsrvutil -v"' % prefix,
-                                suggest_filename="%s_version" % sid)
+                self.add_cmd_output('%s "hdbsrvutil -v"' % prefix,
+                                    suggest_filename="%s_version" % sid)
 
-            self.add_cmd_output('%s \'hdbcons "mm l -s -S -p"\'' % prefix,
-                                suggest_filename="%s_memusage" % sid)
+                self.add_cmd_output('%s \'hdbcons "mm l -s -S -p"\'' % prefix,
+                                    suggest_filename="%s_memusage" % sid)
 
-            self.add_cmd_output('%s \'hdbcons -e hdbindexserver \
-                                "replication info"\'' % prefix,
-                                suggest_filename="%s_replicainfo" % sid)
+                self.add_cmd_output('%s \'hdbcons -e hdbindexserver \
+                                    "replication info"\'' % prefix,
+                                    suggest_filename="%s_replicainfo" % sid)
 
-            i = os.listdir("/hana/shared/%s/" % sid)
-            for inst in i:
-                if "HDB" in inst:
-                    inst = inst.strip()[-2:]
+                if os.path.isdir("/hana/shared/%s/" % sid):
+                    i = os.listdir("/hana/shared/%s/" % sid)
+                    for inst in i:
+                        if "HDB" in inst:
+                            inst = inst.strip()[-2:]
 
-                    # get GREEN/RED status
-                    self.add_cmd_output(
-                        'su - %s -c "sapcontrol -nr %s \
-                        -function GetProcessList"'
-                        % (sidadm, inst),
-                        suggest_filename="%s_%s_status"
-                        % (sid, inst))
+                            # get GREEN/RED status
+                            self.add_cmd_output(
+                                'su - %s -c "sapcontrol -nr %s \
+                                -function GetProcessList"'
+                                % (sidadm, inst),
+                                suggest_filename="%s_%s_status"
+                                % (sid, inst))
 
-                    path = '/usr/sap/%s/HDB%s/exe/python_support' % (sid, inst)
-                    # SCALE OUT - slow
-                    self.add_cmd_output(
-                        'su - %s -c "python \
-                        %s/landscapeHostConfiguration.py"'
-                        % (sidadm, path),
-                        suggest_filename="%s_%s_landscapeHostConfig"
-                        % (sid, inst))
+                            path = '/usr/sap/%s/HDB%s/exe/python_support' \
+                                % (sid, inst)
+
+                            if os.path.isdir("%s" % path):
+                                # SCALE OUT - slow
+                                self.add_cmd_output(
+                                    'su - %s -c "python \
+                                    %s/landscapeHostConfiguration.py"'
+                                    % (sidadm, path),
+                                    suggest_filename="%s_%s_landscapeConfig"
+                                    % (sid, inst))
 
 # vim: et ts=4 sw=4
