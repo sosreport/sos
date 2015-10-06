sosreport/sos

commit 1699eabd13ed9f0decb5178298d2fa5d2add43c3

[openstack] Remove 'OpenStack' from distro class names.
Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

sos/plugins/openstack_cinder.py
@@ -60,7 +60,7 @@ class OpenStackCinder(Plugin):
         self.do_path_regex_sub("/etc/cinder/*", regexp, r"\1*********")
 
 
-class DebianOpenStackCinder(OpenStackCinder, DebianPlugin, UbuntuPlugin):
+class DebianCinder(OpenStackCinder, DebianPlugin, UbuntuPlugin):
 
     cinder = False
     packages = (
@@ -81,7 +81,7 @@ class DebianOpenStackCinder(OpenStackCinder, DebianPlugin, UbuntuPlugin):
         super(DebianOpenStackCinder, self).setup()
 
 
-class RedHatOpenStackCinder(OpenStackCinder, RedHatPlugin):
+class RedHatCinder(OpenStackCinder, RedHatPlugin):
 
     cinder = False
     packages = ('openstack-cinder',

sos/plugins/openstack_horizon.py
@@ -43,7 +43,7 @@ class OpenStackHorizon(Plugin):
                                regexp, r"\1*********")
 
 
-class DebianOpenStackHorizon(OpenStackHorizon, DebianPlugin):
+class DebianHorizon(OpenStackHorizon, DebianPlugin):
 
     packages = (
         'python-django-horizon',
@@ -56,7 +56,7 @@ class DebianOpenStackHorizon(OpenStackHorizon, DebianPlugin):
         self.add_copy_spec("/etc/apache2/sites-available/")
 
 
-class UbuntuOpenStackHorizon(OpenStackHorizon, UbuntuPlugin):
+class UbuntuHorizon(OpenStackHorizon, UbuntuPlugin):
 
     packages = (
         'python-django-horizon',
@@ -69,7 +69,7 @@ class UbuntuOpenStackHorizon(OpenStackHorizon, UbuntuPlugin):
         self.add_copy_spec("/etc/apache2/conf.d/openstack-dashboard.conf")
 
 
-class RedHatOpenStackHorizon(OpenStackHorizon, RedHatPlugin):
+class RedHatHorizon(OpenStackHorizon, RedHatPlugin):
 
     packages = (
         'python-django-horizon',

sos/plugins/openstack_nova.py
@@ -75,7 +75,7 @@ class OpenStackNova(Plugin):
         self.do_path_regex_sub("/etc/nova/*", regexp, r"\1*********")
 
 
-class DebianOpenStackNova(OpenStackNova, DebianPlugin, UbuntuPlugin):
+class DebianNova(OpenStackNova, DebianPlugin, UbuntuPlugin):
 
     nova = False
     packages = (
@@ -111,7 +111,7 @@ class DebianOpenStackNova(OpenStackNova, DebianPlugin, UbuntuPlugin):
         self.add_copy_spec(["/etc/sudoers.d/nova_sudoers"])
 
 
-class RedHatOpenStackNova(OpenStackNova, RedHatPlugin):
+class RedHatNova(OpenStackNova, RedHatPlugin):
 
     nova = False
     packages = (

sos/plugins/openstack_sahara.py
@@ -42,7 +42,7 @@ class OpenStackSahara(Plugin):
         self.do_path_regex_sub("/etc/sahara/*", regexp, r"\1*********")
 
 
-class DebianOpenStackSahara(OpenStackSahara, DebianPlugin, UbuntuPlugin):
+class DebianSahara(OpenStackSahara, DebianPlugin, UbuntuPlugin):
     """OpenStackSahara related information for Debian based distributions."""
 
     packages = (
@@ -57,7 +57,7 @@ class DebianOpenStackSahara(OpenStackSahara, DebianPlugin, UbuntuPlugin):
         super(DebianOpenStackSahara, self).setup()
 
 
-class RedHatOpenStackSahara(OpenStackSahara, RedHatPlugin):
+class RedHatSahara(OpenStackSahara, RedHatPlugin):
     """OpenStack sahara related information for Red Hat distributions."""
 
     packages = (

sos/plugins/openstack_trove.py
@@ -44,7 +44,7 @@ class OpenStackTrove(Plugin):
         self.do_path_regex_sub("/etc/trove/*", regexp, r"\1*********")
 
 
-class DebianOpenStackTrove(OpenStackTrove, DebianPlugin, UbuntuPlugin):
+class DebianTrove(OpenStackTrove, DebianPlugin, UbuntuPlugin):
 
     packages = [
         'python-trove',
@@ -57,7 +57,7 @@ class DebianOpenStackTrove(OpenStackTrove, DebianPlugin, UbuntuPlugin):
         super(DebianOpenStackTrove, self).setup()
 
 
-class RedHatOpenStackTrove(OpenStackTrove, RedHatPlugin):
+class RedHatTrove(OpenStackTrove, RedHatPlugin):
 
     packages = ['openstack-trove']
 

sos/plugins/openstack_ceilometer.py
@@ -48,8 +48,8 @@ class OpenStackCeilometer(Plugin):
         self.do_path_regex_sub("/etc/ceilometer/*", regexp, r"\1*********")
 
 
-class DebianOpenStackCeilometer(OpenStackCeilometer, DebianPlugin,
-                                UbuntuPlugin):
+class DebianCeilometer(OpenStackCeilometer, DebianPlugin,
+                       UbuntuPlugin):
 
     packages = (
         'ceilometer-api',
@@ -62,7 +62,7 @@ class DebianOpenStackCeilometer(OpenStackCeilometer, DebianPlugin,
     )
 
 
-class RedHatOpenStackCeilometer(OpenStackCeilometer, RedHatPlugin):
+class RedHatCeilometer(OpenStackCeilometer, RedHatPlugin):
 
     packages = (
         'openstack-ceilometer',

sos/plugins/openstack_keystone.py
@@ -48,7 +48,7 @@ class OpenStackKeystone(Plugin):
         self.do_path_regex_sub("/etc/keystone/*", regexp, r"\1*********")
 
 
-class DebianOpenStackKeystone(OpenStackKeystone, DebianPlugin, UbuntuPlugin):
+class DebianKeystone(OpenStackKeystone, DebianPlugin, UbuntuPlugin):
 
     packages = (
         'keystone',
@@ -57,7 +57,7 @@ class DebianOpenStackKeystone(OpenStackKeystone, DebianPlugin, UbuntuPlugin):
     )
 
 
-class RedHatOpenStackKeystone(OpenStackKeystone, RedHatPlugin):
+class RedHatKeystone(OpenStackKeystone, RedHatPlugin):
 
     packages = (
         'openstack-keystone',

sos/plugins/openstack_swift.py
@@ -45,7 +45,7 @@ class OpenStackSwift(Plugin):
         self.do_path_regex_sub("/etc/swift/.*\.conf.*", regexp, r"\1*********")
 
 
-class DebianOpenStackSwift(OpenStackSwift, DebianPlugin, UbuntuPlugin):
+class DebianSwift(OpenStackSwift, DebianPlugin, UbuntuPlugin):
 
     packages = (
         'swift',
@@ -59,7 +59,7 @@ class DebianOpenStackSwift(OpenStackSwift, DebianPlugin, UbuntuPlugin):
     )
 
 
-class RedHatOpenStackSwift(OpenStackSwift, RedHatPlugin):
+class RedHatSwift(OpenStackSwift, RedHatPlugin):
 
     packages = (
         'openstack-swift',

commit 2e1c0d8c24029fb722298ceb61e8dae99d437d81

[openstack_*] fix setup() regressions introduced in 1699eabd
1699eabd partially renamed various OpenStack plugin classes but
was not complete when it was pulled in leading to various
openstack plugins failing during setup.

Resolves: #655

Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

sos/plugins/openstack_cinder.py
@@ -78,7 +78,7 @@ class DebianCinder(OpenStackCinder, DebianPlugin, UbuntuPlugin):
         return self.cinder
 
     def setup(self):
-        super(DebianOpenStackCinder, self).setup()
+        super(DebianCinder, self).setup()
 
 
 class RedHatCinder(OpenStackCinder, RedHatPlugin):
@@ -93,7 +93,7 @@ class RedHatCinder(OpenStackCinder, RedHatPlugin):
         return self.cinder
 
     def setup(self):
-        super(RedHatOpenStackCinder, self).setup()
+        super(RedHatCinder, self).setup()
         self.add_copy_spec(["/etc/sudoers.d/cinder"])
 
 

sos/plugins/openstack_horizon.py
@@ -54,7 +54,7 @@ class DebianHorizon(OpenStackHorizon, DebianPlugin):
     )
 
     def setup(self):
-        super(DebianOpenStackHorizon, self).setup()
+        super(DebianHorizon, self).setup()
         self.add_copy_spec("/etc/apache2/sites-available/")
 
 
@@ -67,7 +67,7 @@ class UbuntuHorizon(OpenStackHorizon, UbuntuPlugin):
     )
 
     def setup(self):
-        super(UbuntuOpenStackHorizon, self).setup()
+        super(UbuntuHorizon, self).setup()
         self.add_copy_spec("/etc/apache2/conf.d/openstack-dashboard.conf")
 
 
@@ -79,7 +79,7 @@ class RedHatHorizon(OpenStackHorizon, RedHatPlugin):
     )
 
     def setup(self):
-        super(RedHatOpenStackHorizon, self).setup()
+        super(RedHatHorizon, self).setup()
         self.add_copy_spec("/etc/httpd/conf.d/openstack-dashboard.conf")
         if self.get_option("log"):
             self.add_copy_spec("/var/log/httpd/")

sos/plugins/openstack_instack.py
@@ -62,7 +62,7 @@ class OpenStackInstack(Plugin):
                          r"\1*********")
 
 
-class RedHatOpenStackRDOManager(OpenStackInstack, RedHatPlugin):
+class RedHatRDOManager(OpenStackInstack, RedHatPlugin):
 
     packages = [
         'instack',
@@ -70,6 +70,6 @@ class RedHatOpenStackRDOManager(OpenStackInstack, RedHatPlugin):
     ]
 
     def setup(self):
-        super(RedHatOpenStackRDOManager, self).setup()
+        super(RedHatRDOManager, self).setup()
 
 # vim: set et ts=4 sw=4 :

sos/plugins/openstack_ironic.py
@@ -42,7 +42,7 @@ class OpenStackIronic(Plugin):
             self.do_path_regex_sub(conf, regexp, r"\1*********")
 
 
-class DebianOpenStackIronic(OpenStackIronic, DebianPlugin, UbuntuPlugin):
+class DebianIronic(OpenStackIronic, DebianPlugin, UbuntuPlugin):
 
     packages = [
         'ironic-api',
@@ -51,10 +51,10 @@ class DebianOpenStackIronic(OpenStackIronic, DebianPlugin, UbuntuPlugin):
     ]
 
     def setup(self):
-        super(DebianOpenStackIronic, self).setup()
+        super(DebianIronic, self).setup()
 
 
-class RedHatOpenStackIronic(OpenStackIronic, RedHatPlugin):
+class RedHatIronic(OpenStackIronic, RedHatPlugin):
 
     packages = [
         'openstack-ironic-api',
@@ -68,7 +68,7 @@ class RedHatOpenStackIronic(OpenStackIronic, RedHatPlugin):
     ]
 
     def setup(self):
-        super(RedHatOpenStackIronic, self).setup()
+        super(RedHatIronic, self).setup()
 
         # is the optional ironic-discoverd service installed?
         if any([self.is_installed(p) for p in self.discoverd_packages]):

sos/plugins/openstack_neutron.py
@@ -27,7 +27,7 @@ from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
 # info...
 
 
-class Neutron(Plugin):
+class OpenStackNeutron(Plugin):
     """OpenStack Networking
     """
     plugin_name = "openstack_neutron"
@@ -131,7 +131,7 @@ class Neutron(Plugin):
         return tuple(names)
 
 
-class DebianNeutron(Neutron, DebianPlugin, UbuntuPlugin):
+class DebianNeutron(OpenStackNeutron, DebianPlugin, UbuntuPlugin):
     package_list_template = [
         '%(comp)s-common',
         '%(comp)s-plugin-cisco',
@@ -155,7 +155,7 @@ class DebianNeutron(Neutron, DebianPlugin, UbuntuPlugin):
         self.add_copy_spec("/etc/sudoers.d/%s_sudoers" % self.component_name)
 
 
-class RedHatNeutron(Neutron, RedHatPlugin):
+class RedHatNeutron(OpenStackNeutron, RedHatPlugin):
 
     package_list_template = [
         'openstack-%(comp)s',

sos/plugins/openstack_nova.py
@@ -111,7 +111,7 @@ class DebianNova(OpenStackNova, DebianPlugin, UbuntuPlugin):
         return self.nova
 
     def setup(self):
-        super(DebianOpenStackNova, self).setup()
+        super(DebianNova, self).setup()
         self.add_copy_spec(["/etc/sudoers.d/nova_sudoers"])
 
 
@@ -141,7 +141,7 @@ class RedHatNova(OpenStackNova, RedHatPlugin):
         return self.nova
 
     def setup(self):
-        super(RedHatOpenStackNova, self).setup()
+        super(RedHatNova, self).setup()
         self.add_copy_spec([
             "/etc/logrotate.d/openstack-nova",
             "/etc/polkit-1/localauthority/50-local.d/50-nova.pkla",

sos/plugins/openstack_sahara.py
@@ -54,7 +54,7 @@ class DebianSahara(OpenStackSahara, DebianPlugin, UbuntuPlugin):
     )
 
     def setup(self):
-        super(DebianOpenStackSahara, self).setup()
+        super(DebianSahara, self).setup()
 
 
 class RedHatSahara(OpenStackSahara, RedHatPlugin):
@@ -66,7 +66,7 @@ class RedHatSahara(OpenStackSahara, RedHatPlugin):
     )
 
     def setup(self):
-        super(RedHatOpenStackSahara, self).setup()
+        super(RedHatSahara, self).setup()
         self.add_copy_spec("/etc/sudoers.d/sahara")
 
 

sos/plugins/openstack_trove.py
@@ -54,7 +54,7 @@ class DebianTrove(OpenStackTrove, DebianPlugin, UbuntuPlugin):
     ]
 
     def setup(self):
-        super(DebianOpenStackTrove, self).setup()
+        super(DebianTrove, self).setup()
 
 
 class RedHatTrove(OpenStackTrove, RedHatPlugin):
@@ -62,6 +62,6 @@ class RedHatTrove(OpenStackTrove, RedHatPlugin):
     packages = ['openstack-trove']
 
     def setup(self):
-        super(RedHatOpenStackTrove, self).setup()
+        super(RedHatTrove, self).setup()
 
 # vim: set et ts=4 sw=4 :