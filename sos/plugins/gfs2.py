e4ea92277db4fb04f8097b63bae0643ae4288004
Shane Bradley <sbradley@redhat.com>
2016-03-24 09:55:55 -0400

    [cluster] The plugin was removed and split into multiple plugins
    The cluster plugin originally was used for all the components for clustering:
    corosync, cman, pacemaker, gfs2, and dlm. This patch moves the command
    output/file that was captured in the cluster plugin to the corresponding plugin
    related to the command output or file. The cluster plugin was removed.
    
    Each component that is used by the 2 different types of clusters have a
    corresponding plugin: corosync, cman, pacemaker, gfs2, and dlm.
    
    The 2 different types of a cluster are: cman (with or without rgmanager or
    pacemaker managing resources) and pacemaker based clusters. Both of these
    cluster types can use corosync, dlm, and gfs2. The main difference is the
    management of clustered resources either by rgmanager(cman only) or pacemaker.
    The cman plugin will capture data related to rgmanager.
    
    In addition, there were duplicate commands that were removed and a couple of
    commands and file captures that were added to different plugins that previously
    was not in the cluster plugin.
    
    Fixes: #587
    
    Signed-off-by: Shane Bradley <sbradley@redhat.com>

---
+++ sos/plugins/gfs2.py
@@ -0,0 +1,65 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
+import os.path
+
+
+class Gfs2(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
+    """GFS2 (Global Filesystem 2)"""
+
+    plugin_name = "gfs2"
+    profiles = ("cluster", )
+    packages = ["gfs2-utils"]
+
+    option_list = [
+        ("lockdump",
+         "capture lock dumps for all GFS2 filesystems",
+         "slow", False),
+    ]
+
+    debugfs_path = "/sys/kernel/debug"
+    _debugfs_cleanup = False
+
+    def setup(self):
+        self.add_copy_spec([
+            "/sys/fs/gfs2/*/withdraw"
+        ])
+        self.add_cmd_output([
+            "gfs_control ls -n",
+            "gfs_control dump"
+        ])
+
+        if self.get_option("gfs2lockdump"):
+            if self._mount_debug():
+                self.add_copy_spec(["/sys/kernel/debug/gfs2/*"])
+
+    def _mount_debug(self):
+        if not os.path.ismount(self.debugfs_path):
+            self._debugfs_cleanup = True
+            r = self.call_ext_prog("mount -t debugfs debugfs %s"
+                                   % self.debugfs_path)
+            if r["status"] != 0:
+                self._log_error("debugfs not mounted and mount attempt failed")
+                self._debugfs_cleanup = False
+        return os.path.ismount(self.debugfs_path)
+
+    def postproc(self):
+        if self._debugfs_cleanup and os.path.ismount(self.debugfs_path):
+            r = self.call_ext_prog("umount %s" % self.debugfs_path)
+            if r["status"] != 0:
+                self._log_error("could not unmount %s" % self.debugfs_path)
+        return
+
+# vim: et ts=4 sw=4
--- sos/plugins/pacemaker.py
+++ sos/plugins/pacemaker.py
@@ -13,20 +13,58 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
+from datetime import datetime, timedelta
+import re
 
 
 class Pacemaker(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
-    "HA Cluster resource manager"
+    """HA Cluster resource manager"""
 
     plugin_name = "pacemaker"
     profiles = ("cluster", )
-    packages = ("pacemaker", )
+    packages = ["pacemaker"]
+
+    option_list = [
+        ("crm_from", "specify the start time for crm_report", "fast", False),
+        ("crm_scrub", "enable password scrubbing for crm_report", "", True),
+    ]
 
     def setup(self):
         self.add_copy_spec([
-            "/var/lib/pacemaker/cib/cib.xml"
+            "/var/lib/pacemaker/cib/cib.xml",
+            "/etc/sysconfig/pacemaker",
+            "/var/log/pacemaker.log",
+            "/var/log/pcsd/pcsd.log"
         ])
         self.add_cmd_output([
+            "crm_mon -1 -A -n -r -t",
             "crm status",
             "crm configure show",
+            "pcs config",
+            "pcs status",
+            "pcs property list --all"
         ])
+        # crm_report needs to be given a --from "YYYY-MM-DD HH:MM:SS" start
+        # time in order to collect data.
+        crm_from = (datetime.today() -
+                    timedelta(hours=72)).strftime("%Y-%m-%d %H:%m:%S")
+        if self.get_option("crm_from") is not False:
+            if re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',
+                        str(self.get_option("crm_from"))):
+                crm_from = self.get_option("crm_from")
+            else:
+                self._log_error(
+                    "crm_from parameter '%s' is not a valid date: using "
+                    "default" % self.get_option("crm_from"))
+
+        crm_dest = self.get_cmd_output_path(name="crm_report", make=False)
+        crm_scrub = '-p "passw.*"'
+        if not self.get_option("crm_scrub"):
+            crm_scrub = ""
+            self._log_warn("scrubbing of crm passwords has been disabled:")
+            self._log_warn("data collected by crm_report may contain"
+                           " sensitive values.")
+        self.add_cmd_output('crm_report %s -S -d --dest %s --from "%s"' %
+                            (crm_scrub, crm_dest, crm_from),
+                            chroot=self.tmp_in_sysroot())
+# vim: et ts=4 sw=4
