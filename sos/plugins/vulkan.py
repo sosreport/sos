57ef47b52fc8c6eadb6f9d6810bb2def46ba79f1
Bryan Quigley <bryan.quigley@canonical.com>
2016-04-01 16:10:31 -0400

    [x11|opengl|opencl|vulkan] Create seperate plugin for each gpu api
    This takes glxinfo out of x11 and moves it to opengl.
    It also provides clinfo and vulkaninfo in their own plugin.
    
    Signed-off-by: Bryan Quigley <bryan.quigley@canonical.com>

--- sos/plugins/vulkan.py
+++ sos/plugins/vulkan.py
@@ -15,25 +15,17 @@
 from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
 
 
-class X11(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
-    """X windowing system
+class Vulkan(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
+    """Vulkan
     """
 
-    plugin_name = 'x11'
-    profiles = ('hardware', 'desktop')
-
-    files = ('/etc/X11',)
+    plugin_name = 'vulkan'
+    profiles = ('hardware', 'desktop', 'gpu')
+    files = ('/usr/bin/vulkaninfo',)
 
     def setup(self):
-        self.add_copy_spec([
-            "/etc/X11",
-            "/var/log/Xorg.*.log",
-            "/var/log/XFree86.*.log",
-        ])
-        self.add_forbidden_path("/etc/X11/X")
-        self.add_forbidden_path("/etc/X11/fontpath.d")
         self.add_cmd_output([
-            "xrandr --verbose"
+            "vulkaninfo",
         ])
 
 # vim: set et ts=4 sw=4 :
