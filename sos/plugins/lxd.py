d2cf54881a4a4e3d676dff5e2ccd182ce6591648
Jorge Niedbalski <jorge.niedbalski@canonical.com>
2016-05-19 14:23:42 -0400

    [plugins] New LXD plugin
    This ubuntu-specific plugin gathers logs, internal database
    and a list of the current containers on a LXD host.
    
    Signed-off-by: Jorge Niedbalski <jorge.niedbalski@canonical.com>
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

---
+++ sos/plugins/lxd.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2016 Jorge Niedbalski <niedbalski@ubuntu.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from sos.plugins import Plugin, UbuntuPlugin
+
+
+class LXD(Plugin, UbuntuPlugin):
+    """LXD is a containers hypervisor.
+    """
+    plugin_name = 'lxd'
+
+    def setup(self):
+        self.add_copy_spec([
+            "/var/lib/lxd/lxd.db",
+            "/etc/default/lxc-bridge",
+        ])
+
+        self.add_copy_spec([
+            "/var/log/lxd*"
+        ], sizelimit=self.get_option("log_size"))
+
+        # List of containers available on the machine
+        self.add_cmd_output([
+            "lxc list",
+            "lxc profile list",
+            "lxc image list",
+        ])
+
+        self.add_cmd_output([
+            "find /var/lib/lxd -maxdepth 2 -type d -ls",
+        ], suggest_filename='var-lxd-dirs.txt')
+
+# vim: set et ts=4 sw=4 :
