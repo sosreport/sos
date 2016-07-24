57a9b5d294fd2ac637f81982b5ae30c2b3ab864a
Yedidyah Bar David <didi@redhat.com>
2016-07-24 17:38:51 +0300

    [yum] [dnf] add output of history info
    Add a list of installed/erased/updated packages per yum/dnf transaction.
    
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- sos/plugins/yum.py
+++ sos/plugins/yum.py
@@ -27,7 +27,8 @@ class Yum(Plugin, RedHatPlugin):
 
     option_list = [
         ("yumlist", "list repositories and packages", "slow", False),
-        ("yumdebug", "gather yum debugging data", "slow", False)
+        ("yumdebug", "gather yum debugging data", "slow", False),
+        ("yum-history-info", "gather yum history info", "slow", False),
     ]
 
     def setup(self):
@@ -51,6 +52,20 @@ class Yum(Plugin, RedHatPlugin):
             "/etc/pki/entitlement/*.pem"
         ])
         self.add_cmd_output("yum history")
+
+        # packages installed/erased/updated per transaction
+        if self.get_option("yum-history-info"):
+            history = self.call_ext_prog("yum history")
+            transactions = None
+            if history['output']:
+                for line in history['output'].splitlines():
+                    try:
+                        transactions = int(line.split('|')[0].strip())
+                        break
+                    except ValueError:
+                        pass
+            for tr_id in range(1, transactions+1):
+                self.add_cmd_output("yum history info %d" % tr_id)
 
         if self.get_option("yumlist"):
             # List various information about available packages
