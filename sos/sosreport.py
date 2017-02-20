sosreport/sos

commit a2dc73460f37ec832da9867a3e5d8f97d4ec61f8

[sosreport] clarify in man and help that --logsize is in MiB
Neither sosreport --help or man pages specify the units of
--logsize parameter. Let it clarify.

Resolves: #916

Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

sos/sosreport.py
@@ -600,7 +600,7 @@ class SoSOptions(object):
                           default=deque())
         parser.add_option("--log-size", action="store",
                           dest="log_size", default=10, type="int",
-                          help="set a limit on the size of collected logs")
+                          help="set a limit on the size of collected logs (in MiB)")
         parser.add_option("-a", "--alloptions", action="store_true",
                           dest="usealloptions", default=False,
                           help="enable all options for loaded plugins")

man/en/sosreport.1
@@ -107,7 +107,7 @@ testing or other plugin defined behaviour. Use of \--verify may cause
 the time taken to generate a report to be considerably longer.
 .TP
 .B \--log-size
-Places a global limit on the size of any collected set of logs. The
+Places a global limit on the size (in MiB) of any collected set of logs. The
 limit is applied separately for each set of logs collected by any
 plugin.
 .TP

commit c0e5674b1aec848f402f294ee3606a467483a805

[sosreport] fix line length regression (a2dc734)
Fix a pep8 regression introduced in commit a2dc734.

Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/sosreport.py
@@ -600,7 +600,8 @@ class SoSOptions(object):
                           default=deque())
         parser.add_option("--log-size", action="store",
                           dest="log_size", default=10, type="int",
-                          help="set a limit on the size of collected logs (in MiB)")
+                          help="set a limit on the size of collected logs "
+                               "(in MiB)")
         parser.add_option("-a", "--alloptions", action="store_true",
                           dest="usealloptions", default=False,
                           help="enable all options for loaded plugins")