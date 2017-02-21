c9f661107c67244c2b7d4822bb344c54df67b058
Bryan Quigley <bryan.quigley@canonical.com>
2017-02-21 11:43:33 -0500

    [policies/debian] Remove glob in command that causes directories to be queried.
    The change in how commands are parsed was introduced in commit
    04d412816de2e4053f43956adf536272a2ddc19c.  This caused our old dpkg parsing
    to not work.  Whatever the reason for the \* in the command it doesn't appear to
    be needed now anyway so removing it.
    
    Resolves: #935
    
    Signed-off-by: Bryan Quigley <bryan.quigley@canonical.com>

--- sos/policies/debian.py
+++ sos/policies/debian.py
@@ -11,7 +11,7 @@ class DebianPolicy(LinuxPolicy):
     report_name = ""
     ticket_number = ""
     package_manager = PackageManager(
-        "dpkg-query -W -f='${Package}|${Version}\\n' \*")
+        "dpkg-query -W -f='${Package}|${Version}\\n'")
     valid_subclasses = [DebianPlugin]
     PATH = "/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games" \
            + ":/usr/local/sbin:/usr/local/bin"
@@ -21,7 +21,7 @@ class DebianPolicy(LinuxPolicy):
         self.report_name = ""
         self.ticket_number = ""
         self.package_manager = PackageManager(
-            "dpkg-query -W -f='${Package}|${Version}\\n' \*")
+            "dpkg-query -W -f='${Package}|${Version}\\n'")
         self.valid_subclasses = [DebianPlugin]
 
     @classmethod
