88c4be92eb5e389f98ee4dc82ab8981e85115af3
Pavel Moravec <pmoravec@redhat.com>
2017-03-24 22:15:12 +0100

    [numa] missing import to os.path.join command
    "import os.path" missing.
    
    Resolves: #967
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/numa.py
+++ sos/plugins/numa.py
@@ -14,6 +14,7 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 from sos.plugins import Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin
+import os.path
 
 
 class Numa(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
