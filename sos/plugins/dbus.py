18ab06824dfc91f868cbfaac5f204bbf78713d35
Bryn M. Reeves <bmr@redhat.com>
2017-08-31 14:36:15 +0100

    [dbus] add missing packages list
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/dbus.py
+++ sos/plugins/dbus.py
@@ -20,6 +20,7 @@ class Dbus(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
 
     plugin_name = "dbus"
     profiles = ('system',)
+    packages = ('dbus',)
 
     def setup(self):
         self.add_copy_spec([
