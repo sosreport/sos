0bdf74ac0481dc952c22a7dbad5ed35c1516483f
Pavel Moravec <pmoravec@redhat.com>
2017-09-24 12:37:55 +0200

    [vdo] New plugin for Virtual Data Optimizer
    Collect configs and vdo status.
    
    Resolves: #1110
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

---
+++ sos/plugins/vdo.py
@@ -0,0 +1,40 @@
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it would be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write the Free Software Foundation,
+# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+from sos.plugins import Plugin, RedHatPlugin
+
+
+class Vdo(Plugin, RedHatPlugin):
+    """Virtual Data Optimizer
+    """
+
+    plugin_name = 'vdo'
+    profiles = ('storage',)
+    packages = ('vdo',)
+    files = (
+        '/sys/vdo',
+        '/sys/albireo',
+        '/etc/vdoconf.yml',
+        '/etc/vdoconf.xml'
+    )
+
+    def setup(self):
+        self.add_copy_spec(self.files)
+        self.add_cmd_output("vdo status")
+
+# vim set et ts=4 sw=4 :
