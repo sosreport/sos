6013308e2ebfc45d72ef87c3d18fb2edce61d549
Pavel Moravec <pmoravec@redhat.com>
2017-11-09 13:10:15 +0100

    [opendaylight] new plugin for OpenDaylight
    Collecting logs and configs.
    
    Resolves: #1143
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

---
+++ sos/plugins/opendaylight.py
@@ -0,0 +1,38 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from sos.plugins import Plugin, RedHatPlugin
+
+
+class OpenDaylight(Plugin, RedHatPlugin):
+    """OpenDaylight network manager
+    """
+
+    plugin_name = 'opendaylight'
+    profiles = ('openstack', 'openstack_controller')
+
+    packages = ('opendaylight',)
+
+    def setup(self):
+        self.add_copy_spec("/opt/opendaylight/etc/")
+
+        self.limit = self.get_option("log_size")
+        if self.get_option("all_logs"):
+            self.add_copy_spec("/opt/opendaylight/data/log/",
+                               sizelimit=self.limit)
+        else:
+            self.add_copy_spec("/opt/opendaylight/data/log/*log",
+                               sizelimit=self.limit)
+
+# vim: set et ts=4 sw=4 :
