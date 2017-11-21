sosreport/sos

commit d0bcd552f4bc2119afd2a835f4e5ec1107421f40

[openstack_horizon] fix regex in postproc method
"The regex needs to handle in openstack_horizon:
currently the configurations are listed by
"/etc/openstack-dashboard/local_settings"
causing below errors:

regex substitution failed for '/etc/openstack-dashboard/local_settings.d/_11_rcue_theme.pyc'

Purposing a fix by changing the regex to match only intended configuration
files (*.conf*) in this directory.

Signed-off-by: Poornima M. Kshirsagar pkshiras@redhat.com

sos/plugins/openstack_horizon.py
@@ -48,7 +48,8 @@ class OpenStackHorizon(Plugin):
         regexp = r"((?m)^\s*(%s)\s*=\s*)(.*)" % "|".join(protect_keys)
         self.do_path_regex_sub("/etc/openstack-dashboard/.*\.json",
                                regexp, r"\1*********")
-        self.do_path_regex_sub("/etc/openstack-dashboard/local_settings",
+        self.do_path_regex_sub("/etc/openstack-dashboard/local_settings/ + \
+                               .*\.conf.*",
                                regexp, r"\1*********")
 
 

commit ca0f2d4c672d9b65b7a5eaf6b055e958e3f636fd

[openstack_horizon] fix password obfuscate in local_settings
commit d0bcd552f4bc2119afd2a835f4e5ec1107421f40 broke the
replacement of the protect_keys for the main local_settings
config file.

Signed-off-by: Martin Schuppert mschuppe@redhat.com

sos/plugins/openstack_horizon.py
@@ -74,11 +74,11 @@ class OpenStackHorizon(Plugin):
             regexp, r"\1*********"
         )
         self.do_path_regex_sub(
-            "/etc/openstack-dashboard/local_settings/.*\.conf.*",
+            "/etc/openstack-dashboard/local_settings$",
             regexp, r"\1*********"
         )
         self.do_path_regex_sub(
-            var_puppet_gen + "/etc/openstack-dashboard/.*\.conf.*",
+            var_puppet_gen + "/etc/openstack-dashboard/local_settings$",
             regexp, r"\1*********"
         )
 