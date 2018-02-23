4e26121ada4e5d21de81ebb91b40c6c6ec13f693
Saravanan KR <skramaja@redhat.com>
2018-02-23 15:11:06 +0530

    [os_net_config] Capture the os-net-config run time data
    For DPDK OpenStack deployments with TripleO, os-net-config
    binds the DPDK PMD driver to the interface and writes the
    details in to the directory /var/lib/os-net-config. Adding
    this directory for the report collection.
    
    Resolves: #1224
    
    Signed-off-by: Saravanan KR <skramaja@redhat.com>
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/os_net_config.py
+++ sos/plugins/os_net_config.py
@@ -24,6 +24,7 @@ class OsNetConfig(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
 
     def setup(self):
         self.add_copy_spec("/etc/os-net-config")
+        self.add_copy_spec("/var/lib/os-net-config")
 
 
 # vim: set et ts=4 sw=4 :
