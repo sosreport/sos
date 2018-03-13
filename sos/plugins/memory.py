103414440ce1315fe3bf292c5f4517d1355f4528
Bryn M. Reeves <bmr@redhat.com>
2018-03-13 12:34:37 +0000

    [memory] collect swapon verbose summary
    Fixes: #863
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/memory.py
+++ sos/plugins/memory.py
@@ -37,7 +37,8 @@ class Memory(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
         self.add_cmd_output("free", root_symlink="free")
         self.add_cmd_output([
             "free -m",
-            "swapon --bytes --show"
+            "swapon --bytes --show",
+            "swapon --summary --verbose"
         ])
 
 # vim: set et ts=4 sw=4 :
