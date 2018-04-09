731f6918cac34a5a059aab5787c1ad73b12cdc59
Jake Hunsaker <jhunsake@redhat.com>
2018-04-09 13:51:46 -0400

    [global] Kill license headers
    Removes the existing license header and replaces it with a generic
    header pointing to the LICENSE file.
    
    Existing date/ownership details are left intact where they were already
    used.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- sos/plugins/vdo.py
+++ sos/plugins/vdo.py
@@ -1,20 +1,10 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
 #
-# Copyright (c) 2017 Red Hat, Inc.
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write the Free Software Foundation,
-# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
+# See the LICENSE file in the source distribution for further information.
 
 from sos.plugins import Plugin, RedHatPlugin
 
