sosreport/sos

commit 7ba2fb47d05b608f3863fda9271894e38b43937a

[jars] fix maven_id dictionary encoding
The dictionary must contain string types, not byte arrays.

Causes an exception when packing strings with Python3:

  [sos.sosreport:setup] executing 'sosreport -vvv --batch --debug -o jars'
   Setting up plugins ...
  Traceback (most recent call last):
    File "./sosreport", line 25, in <module>
      main(sys.argv[1:])
    File "/home/breeves/src/git/sos/sos/sosreport.py", line 1632, in main
      sos.execute()
  TypeError: key b'version' is not a string

Resolves: #984.

Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/plugins/__init__.py
@@ -671,7 +671,11 @@ class Plugin(object):
     def add_string_as_file(self, content, filename):
         """Add a string to the archive as a file named `filename`"""
         self.copy_strings.append((content, filename))
-        content = "..." + (content.splitlines()[0]).decode('utf8', 'ignore')
+        if isinstance(content, six.string_types):
+            content = "..." + content.splitlines()[0]
+        else:
+            content = ("..." +
+                       (content.splitlines()[0]).decode('utf8', 'ignore'))
         self._log_debug("added string '%s' as '%s'" % (content, filename))
 
     def get_cmd_output_now(self, exe, suggest_filename=None,
@@ -824,7 +828,11 @@ class Plugin(object):
     def _collect_strings(self):
         for string, file_name in self.copy_strings:
             content = "..."
-            content += (string.splitlines()[0]).decode('utf8', 'ignore')
+            if isinstance(string, six.string_types):
+                content = "..." + content.splitlines()[0]
+            else:
+                content = ("..." +
+                           (content.splitlines()[0]).decode('utf8', 'ignore'))
             self._log_info("collecting string '%s' as '%s'"
                            % (content, file_name))
             try:

sos/plugins/jars.py
@@ -113,7 +113,9 @@ class Jars(Plugin, RedHatPlugin):
                         if not line.startswith(b"#"):
                             try:
                                 (key, value) = line.split(b"=")
-                                props[key.strip()] = value.strip()
+                                key = key.decode('utf8').strip()
+                                value = value.decode('utf8').strip()
+                                props[key] = value
                             except ValueError:
                                 return None
         except IOError:

commit bf8433945aeced7c0d73686ec83d826cc33bbd28

[Plugin] Fix string decoding for debug log output
In add_string_as_file and _collect_strings, the first line of each
string is printed to debug log, however there is a bug in
_collect_strings that tries to decode the first line of '...'
instead of the actual string; this causes a error like:

  File "/usr/share/sosreport/sos/sosreport.py", line 1300, in collect
    plug.collect()
  File "/usr/share/sosreport/sos/plugins/__init__.py", line 877, in collect
    self._collect_strings()
  File "/usr/share/sosreport/sos/plugins/__init__.py", line 860, in _collect_strings
    (content.splitlines()[0]).decode('utf8', 'ignore'))
AttributeError: 'str' object has no attribute 'decode'

This simplifies the (string or bytearray)-to-first-line-string of both
functions, which also fixes the bug using the wrong variable to decode.

Fixes: 7ba2fb47d05b608f3863fda9271894e38b43937a
Signed-off-by: Dan Streetman <ddstreet@canonical.com>

sos/plugins/__init__.py
@@ -709,12 +709,10 @@ class Plugin(object):
     def add_string_as_file(self, content, filename):
         """Add a string to the archive as a file named `filename`"""
         self.copy_strings.append((content, filename))
-        if isinstance(content, six.string_types):
-            content = "..." + content.splitlines()[0]
-        else:
-            content = ("..." +
-                       (content.splitlines()[0]).decode('utf8', 'ignore'))
-        self._log_debug("added string '%s' as '%s'" % (content, filename))
+        content = content.splitlines()[0]
+        if not isinstance(content, six.string_types):
+            content = content.decode('utf8', 'ignore')
+        self._log_debug("added string ...'%s' as '%s'" % (content, filename))
 
     def get_cmd_output_now(self, exe, suggest_filename=None,
                            root_symlink=False, timeout=300, stderr=True,
@@ -863,13 +861,10 @@ class Plugin(object):
 
     def _collect_strings(self):
         for string, file_name in self.copy_strings:
-            content = "..."
-            if isinstance(string, six.string_types):
-                content = "..." + content.splitlines()[0]
-            else:
-                content = ("..." +
-                           (content.splitlines()[0]).decode('utf8', 'ignore'))
-            self._log_info("collecting string '%s' as '%s'"
+            content = string.splitlines()[0]
+            if not isinstance(content, six.string_types):
+                content = content.decode('utf8', 'ignore')
+            self._log_info("collecting string ...'%s' as '%s'"
                            % (content, file_name))
             try:
                 self.archive.add_string(string,