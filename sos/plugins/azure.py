2179ebe3d5010df395db0f1f885e92f411eea107
Dave Mulford <dave.mulford@gmail.com>
2018-04-23 15:06:29 -0500

    [azure] Added /var/log/azure directory to sosreport

--- sos/plugins/azure.py
+++ sos/plugins/azure.py
@@ -14,9 +14,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from sos.plugins import Plugin, UbuntuPlugin, RedHatPlugin
 import os
-
+from sos.plugins import Plugin, UbuntuPlugin, RedHatPlugin
 
 class Azure(Plugin, UbuntuPlugin):
     """ Microsoft Azure client
@@ -35,6 +34,15 @@ class Azure(Plugin, UbuntuPlugin):
             "/sys/module/hv_netvsc/parameters/ring_size",
             "/sys/module/hv_storvsc/parameters/storvsc_ringbuffer_size"
         ])
+
+        # Adds all files under /var/log/azure to the sosreport
+        # os.walk is used here because /var/log/azure is used by multiple Microsoft Azure extensions
+        # and there is no standard log filename format
+        limit = self.get_option("log_size")
+
+        for path, subdirs, files in os.walk("/var/log/azure"):
+            for name in files:
+                self.add_copy_spec(os.path.join(path, name), sizelimit=limit)
 
         self.add_cmd_output((
             'curl -s -H Metadata:true '
