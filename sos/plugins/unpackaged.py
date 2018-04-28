6417f165f27adb578e4e485787bf95524092adc1
Pierguido Lambri <plambri@redhat.com>
2018-04-28 19:38:24 +0100

    [unpackaged] Fixed policy calling
    Plugin.policy is now an object instead of
    a function (see commit 5d41100f9d289121467b436f124f91137bc7c621).
    
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- sos/plugins/unpackaged.py
+++ sos/plugins/unpackaged.py
@@ -66,8 +66,8 @@ class Unpackaged(Plugin, RedHatPlugin):
             return expanded
 
         all_fsystem = []
-        all_frpm = set(self.policy().mangle_package_path(
-                       self.policy().package_manager.files))
+        all_frpm = set(self.policy.mangle_package_path(
+                       self.policy.package_manager.files))
         for d in get_env_path_list():
             all_fsystem += all_files_system(d)
         not_packaged = [x for x in all_fsystem if x not in all_frpm]
