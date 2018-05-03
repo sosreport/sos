1b46b1b00183109b6ad5fda669e8b32df19408a5
Mikel Olasagasti Uranga <mikel@olasagasti.info>
2018-05-03 17:42:22 +0200

    [x11] Add .old files to check previous executions
    Signed-off-by: Mikel Olasagasti Uranga <mikel@olasagasti.info>

--- sos/plugins/x11.py
+++ sos/plugins/x11.py
@@ -28,7 +28,9 @@ class X11(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
         self.add_copy_spec([
             "/etc/X11",
             "/var/log/Xorg.*.log",
+            "/var/log/Xorg.*.log.old",
             "/var/log/XFree86.*.log",
+            "/var/log/XFree86.*.log.old",
         ])
 
         self.add_forbidden_path([
