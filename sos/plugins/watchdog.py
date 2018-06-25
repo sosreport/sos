db7a86e64c7b6e35f3752974381b52be05dd712d
Bryn M. Reeves <bmr@redhat.com>
2018-06-25 15:32:56 +0100

    [unpackaged,watchdog] fix license headers
    Resolves: #1365
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/watchdog.py
+++ sos/plugins/watchdog.py
@@ -1,18 +1,12 @@
 # Copyright (C) 2018 Red Hat, Inc., Reid Wahl <nwahl@redhat.com>
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
 
 from sos.plugins import Plugin, RedHatPlugin
 
