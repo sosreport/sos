6d403718ad4a77528d3cd47bdaf954274325a42e
Pavel Moravec <pmoravec@redhat.com>
2018-08-31 17:19:32 +0200

    [openstack_nova] remove too restrictive check_enabled
    Enable the plugin just based on package presence.
    
    Resolves: #1411
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/plugins/openstack_nova.py
+++ sos/plugins/openstack_nova.py
@@ -195,10 +195,6 @@ class DebianNova(OpenStackNova, DebianPlugin, UbuntuPlugin):
         'python-novnc'
     )
 
-    def check_enabled(self):
-        self.nova = self.is_installed("nova-common")
-        return self.nova
-
     def setup(self):
         super(DebianNova, self).setup()
         self.add_copy_spec([
@@ -227,10 +223,6 @@ class RedHatNova(OpenStackNova, RedHatPlugin):
         'python-novaclient',
         'novnc'
     )
-
-    def check_enabled(self):
-        self.nova = self.is_installed("openstack-nova-common")
-        return self.nova
 
     def setup(self):
         super(RedHatNova, self).setup()
