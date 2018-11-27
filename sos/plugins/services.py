1765621d19efb234ca7f36e2e89435fbd7593471
Bryan Quigley <bryan.quigley@canonical.com>
2018-11-26 17:50:52 -0800

    [services/upstart] Cleanup and move commands to right plugin
    Remove /sbin/ for service command so it runs on ubuntu.
    Move initctl command to upstart plugin
    
    Signed-off-by: Bryan Quigley <bryan.quigley@canonical.com>

--- sos/plugins/services.py
+++ sos/plugins/services.py
@@ -25,7 +25,7 @@ class Services(Plugin):
             "/etc/rc.d"
         ])
         if self.get_option('servicestatus'):
-            self.add_cmd_output("/sbin/service --status-all")
+            self.add_cmd_output("service --status-all")
         self.add_cmd_output([
             "/sbin/runlevel",
             "ls /var/lock/subsys"
@@ -44,10 +44,5 @@ class DebianServices(Services, DebianPlugin, UbuntuPlugin):
     def setup(self):
         super(DebianServices, self).setup()
         self.add_copy_spec("/etc/rc*.d")
-
-        self.add_cmd_output("/sbin/initctl show-config",
-                            root_symlink="initctl")
-        if self.get_option('servicestatus'):
-            self.add_cmd_output("/sbin/initctl list")
 
 # vim: set et ts=4 sw=4 :
