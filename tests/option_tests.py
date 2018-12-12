c8d91b2e6be3c975d24f50f9e480ff176cfa48b1
Bryn M. Reeves <bmr@redhat.com>
2018-12-12 15:41:42 +0000

    [Plugin] clean up Plugin.get_option()
    There's a lot of ancient junk in this method (and associated code
    strewn around sos.sosreport and tests). Remove the ability to pass
    a list of options to the method since nothing uses this, and also
    delete the incomplete implementation of global plugin options via
    the commons dictionary (this work was already completed some time
    ago by mapping these options directly to the command line args).
    
    Resolves: #1498
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- tests/option_tests.py
+++ tests/option_tests.py
@@ -12,27 +12,19 @@ class GlobalOptionTest(unittest.TestCase):
         self.commons = {
             'sysroot': '/',
             'policy': LinuxPolicy(),
-            'global_plugin_options': {
-                'test_option': 'foobar',
-                'baz': None,
-                'empty_global': True
-            },
         }
         self.plugin = Plugin(self.commons)
-        self.plugin.opt_names = ['baz', 'empty']
-        self.plugin.opt_parms = [{'enabled': False}, {'enabled': None}]
+        self.plugin.opt_names = ['baz', 'empty', 'test_option']
+        self.plugin.opt_parms = [
+            {'enabled': False}, {'enabled': None}, {'enabled': 'foobar'}
+        ]
 
     def test_simple_lookup(self):
         self.assertEquals(self.plugin.get_option('test_option'), 'foobar')
 
-    def test_multi_lookup(self):
-        self.assertEquals(self.plugin.get_option(('not_there', 'test_option')), 'foobar')
-
     def test_cascade(self):
         self.assertEquals(self.plugin.get_option(('baz')), False)
 
-    def test_none_should_cascade(self):
-        self.assertEquals(self.plugin.get_option(('empty', 'empty_global')), True)
 
 if __name__ == "__main__":
     unittest.main()
