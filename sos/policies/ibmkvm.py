c565c2f5dec6c77dc2a13f12615229abbad29721
Sachin Patil <psachin@redhat.com>
2019-02-07 18:21:56 +0530

    [policies.ibmkvm] Fix unhandled return statements reported by LGTM
    Two of the return statements were not reachable and the exceptions were directly
    handling `return`[1]
    
    https://lgtm.com/projects/g/sosreport/sos/rev/8cded9af8d1d75f4896699f83ae3ce289337dd6b
    
    Signed-off-by: Sachin Patil <psachin@redhat.com>

--- sos/policies/ibmkvm.py
+++ sos/policies/ibmkvm.py
@@ -38,9 +38,9 @@ class PowerKVMPolicy(RedHatPolicy):
             with open('/etc/ibm_powerkvm-release', 'r') as fp:
                 version_string = fp.read()
                 return version_string[2][0]
-            return False
         except IOError:
-            return False
+            pass
+        return False
 
 
 class ZKVMPolicy(RedHatPolicy):
@@ -63,9 +63,9 @@ class ZKVMPolicy(RedHatPolicy):
             with open('/etc/base-release', 'r') as fp:
                 version_string = fp.read()
                 return version_string.split(' ', 4)[3][0]
-            return False
         except IOError:
-            return False
+            pass
+        return False
 
 
 # vim: set ts=4 sw=4
