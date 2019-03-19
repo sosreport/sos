4401e65fcd8b705f4fb5ac3a9dad969f81b4caab
Soumya Koduri <skoduri@redhat.com>
2019-03-19 20:21:06 +0530

    [nfs-ganesha] Update nfs-ganesha service log locations
    nfs-ganesha service logs are now located at "/var/log/ganesha/"
    directory by default. Updating the same in sos plugin.
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- sos/plugins/nfsganesha.py
+++ sos/plugins/nfsganesha.py
@@ -21,8 +21,7 @@ class NfsGanesha(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
             "/etc/ganesha",
             "/etc/sysconfig/ganesha",
             "/var/run/sysconfig/ganesha",
-            "/var/log/ganesha.log",
-            "/var/log/ganesha-gfapi.log"
+            "/var/log/ganesha/*.log"
         ])
 
         self.add_cmd_output([
