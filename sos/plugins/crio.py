dfc65b705a5ec0f2219af6c9ff59eba84a03ccfc
Pavel Moravec <pmoravec@redhat.com>
2019-03-21 09:57:51 +0100

    [crio] import all plugins referred by CRIO class
    Resolves: #1606
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/plugins/crio.py
+++ sos/plugins/crio.py
@@ -8,7 +8,7 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
-from sos.plugins import Plugin
+from sos.plugins import Plugin, RedHatPlugin, UbuntuPlugin
 
 
 class CRIO(Plugin, RedHatPlugin, UbuntuPlugin):
