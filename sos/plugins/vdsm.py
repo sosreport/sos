22d30ed296a7fb0b8ac27d8e4ca7d7d529441dd1
Irit goihman <igoihman@redhat.com>
2019-04-07 14:14:32 +0300

    [vdsm] capture supervdsmd status

--- sos/plugins/vdsm.py
+++ sos/plugins/vdsm.py
@@ -65,6 +65,7 @@ class Vdsm(Plugin, RedHatPlugin):
         self.add_forbidden_path('/etc/pki/libvirt/private/*')
 
         self.add_cmd_output('service vdsmd status')
+        self.add_cmd_output('service supervdsmd status')
 
         self.add_copy_spec([
             '/tmp/vds_installer*',
