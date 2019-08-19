554746d8ee2e7f7b505fb5d896e33272a18c3793
Goutham Pacha Ravi <gouthampravi@gmail.com>
2019-08-19 14:27:22 -0700

    [openstack_manila] Collect db version and protect passwords
    - It'd be helpful to collect the database version
      used by the deployment for debug.
    - The password protection regex did not include all the
      plaintext password configuration options.
    - Add support to collect package versions for the verify mode
      of report collection.
    
    Signed-off-by: Goutham Pacha Ravi <gouthampravi@gmail.com>

--- sos/plugins/openstack_manila.py
+++ sos/plugins/openstack_manila.py
@@ -21,6 +21,27 @@ class OpenStackManila(Plugin):
     var_puppet_gen = "/var/lib/config-data/puppet-generated/manila"
 
     def setup(self):
+        manila_config_opt = "--config-dir %s/etc/manila/"
+
+        # check eventlet-based service on the baremetal
+        in_ps = self.check_process_by_name("manila-api")
+
+        # check if manila-api is running inside a container
+        # (doesn't matter if running via httpd or as a eventlet based service)
+        in_container = self.running_in_container()
+
+        if in_container:
+            manila_config = manila_config_opt % self.var_puppet_gen
+        else:
+            manila_config = manila_config_opt % ''
+
+        # gather DB version
+        if in_ps or in_container:
+            self.add_cmd_output(
+                "manila-manage " + manila_config + " db version",
+                suggest_filename="manila_db_version"
+            )
+
         self.add_copy_spec([
             "/etc/manila/",
             self.var_puppet_gen + "/etc/manila/",
@@ -43,6 +64,18 @@ class OpenStackManila(Plugin):
                 "/var/log/containers/httpd/manila-api/*log"
             ])
 
+        if self.get_option("verify"):
+            self.add_cmd_output("rpm -V %s" % ' '.join(self.packages))
+
+    def running_in_container(self):
+        for runtime in ["docker", "podman"]:
+            container_status = self.get_command_output(runtime + " ps")
+            if container_status['status'] == 0:
+                for line in container_status['output'].splitlines():
+                    if line.endswith("manila_api"):
+                        return True
+        return False
+
     def apply_regex_sub(self, regexp, subst):
         self.do_path_regex_sub("/etc/manila/*", regexp, subst)
         self.do_path_regex_sub(
@@ -51,12 +84,8 @@ class OpenStackManila(Plugin):
         )
 
     def postproc(self):
-        protect_keys = [
-            "nova_admin_password",  "rabbit_password",  "qpid_password",
-            "password", "netapp_nas_password", "cinder_admin_password",
-            "neutron_admin_password", "service_instance_password",
-            "transport_url"
-        ]
+        protect_keys = [".*password.*", "transport_url",
+                        "hdfs_ssh_pw", "maprfs_ssh_pw"]
         connection_keys = ["connection", "sql_connection"]
 
         self.apply_regex_sub(
