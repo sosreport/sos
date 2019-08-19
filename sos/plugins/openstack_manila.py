3ed870bd9b7e9ceb26cecd453e44d15a2954413f
Goutham Pacha Ravi <gouthampravi@gmail.com>
2019-08-19 14:27:22 -0700

    [openstack_manila] Collect db version and protect passwords
    - It'd be helpful to collect the database version
      used by the deployment for debug.
    - The password protection regex did not include all the
      plaintext password configuration options.
    - Add support to collect package versions for the verify mode
      of report collection.
    
    Resolves: #1759
    
    Signed-off-by: Goutham Pacha Ravi <gouthampravi@gmail.com>
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- sos/plugins/openstack_manila.py
+++ sos/plugins/openstack_manila.py
@@ -21,6 +21,13 @@ class OpenStackManila(Plugin):
     var_puppet_gen = "/var/lib/config-data/puppet-generated/manila"
 
     def setup(self):
+
+        config_dir = "%s/etc/manila" % (
+            self.var_puppet_gen if self.running_in_container() else ''
+        )
+        manila_cmd = "manila-manage --config-dir %s db version" % config_dir
+        self.add_cmd_output(manila_cmd, suggest_filename="manila_db_version")
+
         self.add_copy_spec([
             "/etc/manila/",
             self.var_puppet_gen + "/etc/manila/",
@@ -39,6 +46,15 @@ class OpenStackManila(Plugin):
                 "/var/log/manila/*.log",
             ])
 
+    def running_in_container(self):
+        for runtime in ["docker", "podman"]:
+            container_status = self.get_command_output(runtime + " ps")
+            if container_status['status'] == 0:
+                for line in container_status['output'].splitlines():
+                    if line.endswith("manila_api"):
+                        return True
+        return False
+
     def apply_regex_sub(self, regexp, subst):
         self.do_path_regex_sub("/etc/manila/*", regexp, subst)
         self.do_path_regex_sub(
@@ -47,12 +63,8 @@ class OpenStackManila(Plugin):
         )
 
     def postproc(self):
-        protect_keys = [
-            "nova_admin_password",  "rabbit_password",  "qpid_password",
-            "password", "netapp_nas_password", "cinder_admin_password",
-            "neutron_admin_password", "service_instance_password",
-            "transport_url"
-        ]
+        protect_keys = [".*password.*", "transport_url",
+                        "hdfs_ssh_pw", "maprfs_ssh_pw"]
         connection_keys = ["connection", "sql_connection"]
 
         self.apply_regex_sub(
