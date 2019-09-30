20da73d2f45a8f8a31ea2a9e1842905612bd49c1
Pavel Moravec <pmoravec@redhat.com>
2019-09-30 13:48:08 +0200

    [pulp,katello] move qpid-stat commands to pulp plugin
    These commands are rather pulp-related and they are required to be
    collected also on Satellite6 Capsules where no katello but pulp
    is present.
    
    Resolves: #1805
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/plugins/katello.py
+++ sos/plugins/katello.py
@@ -9,7 +9,7 @@
 # See the LICENSE file in the source distribution for further information.
 
 from sos.plugins import Plugin, RedHatPlugin
-import os.path
+from pipes import quote
 
 
 class Katello(Plugin, RedHatPlugin):
@@ -35,5 +35,32 @@ class Katello(Plugin, RedHatPlugin):
             (opt, cert) for opt in "quc"
         ])
 
+        kat_db = self.build_query_cmd(
+            "select id,name,checksum_type,updated_at from katello_repositories"
+        )
+        self.add_cmd_output(kat_db, suggest_filename="katello_repositories")
+
+        db_size = self.build_query_cmd(
+            "SELECT table_name, pg_size_pretty(total_bytes) AS total, "
+            "pg_size_pretty(index_bytes) AS INDEX , "
+            "pg_size_pretty(toast_bytes) AS toast, pg_size_pretty(table_bytes)"
+            " AS TABLE FROM ( SELECT *, "
+            "total_bytes-index_bytes-COALESCE(toast_bytes,0) AS table_bytes "
+            "FROM (SELECT c.oid,nspname AS table_schema, relname AS "
+            "TABLE_NAME, c.reltuples AS row_estimate, "
+            "pg_total_relation_size(c.oid) AS total_bytes, "
+            "pg_indexes_size(c.oid) AS index_bytes, "
+            "pg_total_relation_size(reltoastrelid) AS toast_bytes "
+            "FROM pg_class c LEFT JOIN pg_namespace n ON "
+            "n.oid = c.relnamespace WHERE relkind = 'r') a) a order by "
+            "total_bytes DESC"
+        )
+        self.add_cmd_output(db_size, suggest_filename="db_table_size")
+
+    def build_query_cmd(self, query):
+        _cmd = "su postgres -c %s"
+        _dbcmd = "psql foreman -c %s"
+        dbq = _dbcmd % quote(query)
+        return _cmd % quote(dbq)
 
 # vim: set et ts=4 sw=4 :
