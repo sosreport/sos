923b679c112f1952ba343779ce13128f37d5eeaf
Pavel Moravec <pmoravec@redhat.com>
2019-10-31 08:29:01 +0100

    [foreman,katello] move relicts of katello plugin to foreman
    Katello plugin does not further collect any katello-related data.
    
    qpid-stat was moved to pulp, (foreman) table sizes to foreman and
    /var/log/httpd/katello-reverse-proxy* belong to foreman-proxy.
    
    Move the latest bit to foreman and drop katello plugin.
    
    Resolves: #1843
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/plugins/foreman.py
+++ sos/plugins/foreman.py
@@ -114,6 +114,10 @@ class Foreman(Plugin):
             "/etc/puppetlabs/puppet/ssl/certs/{}.pem".format(_hostname),
             "/var/lib/puppet/ssl/certs/{}.pem".format(_hostname),
             "/var/log/{}*/foreman-ssl_*_ssl*log[.-]*".format(self.apachepkg),
+            "/var/log/{}*/katello-reverse-proxy_access_ssl.log*".format(
+                self.apachepkg),
+            "/var/log/{}*/katello-reverse-proxy_error_ssl.log*".format(
+                self.apachepkg),
             "/var/log/{}*/error_log*".format(self.apachepkg),
             "/etc/{}*/conf/".format(self.apachepkg),
             "/etc/{}*/conf.d/".format(self.apachepkg)
