25539a3d28de6c5dabe1744525f130d8be62f096
Jose Castillo <jose.mfcastillo@gmail.com>
2020-01-10 12:29:05 +0100

    [convert2rhel] New plugin for convert2rhel tool
    This plugin gathers logfiles that were created
    in /var/log/conver2rhel/ after running the
    convert2rhel tool.
    
    Resolves: #
    Related: RHBZ#1594308
    
    Signed-off-by: Jose Castillo <jose.mfcastillo@gmail.com>

---
+++ sos/plugins/convert2rhel.py
@@ -0,0 +1,27 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.plugins import Plugin, RedHatPlugin
+
+
+class convert2rhel(Plugin, RedHatPlugin):
+
+    plugin_name = 'convert2rhel'
+    profiles = ('system')
+    packages = ('convert2rhel')
+    verify_packages = ('convert2rhel$',)
+
+    def setup(self):
+
+        self.add_copy_spec([
+            "/var/log/convert2rhel/convert2rhel.log",
+            "/var/log/convert2rhel/rpm_va.log"
+        ])
+
+
+# vim: set et ts=4 sw=4 :
