sosreport/sos

commit e51d3e6d5b5c084a3289751dbca559ba134bace8

[plugins] Update plugins to only use new consolidated methods
Updates numerous plugins to no longer use `get_command_output()`,
`call_ext_prog()`, or `check_ext_prog()` - instead only using
`collect_cmd_output()` where the content needs to be saved to the
archive, and `exec_cmd()` where it does not.

For the vast majority of plugins, these calls were able to be changed
in-place with little to no further alteration of plugin code outside of
no longer making indenpendent open() calls to files created, as the
content of those files is now directly available. Exceptions are noted
below.

For the dlm, gfs2, and kvm plugins the only use of get_command_output()
was to mount /sys/kernel/debugfs which in modern distributions is no
longer needed, so those plugins have had that functionality entirely
removed instead of being ported to the new `exec_cmd()`.

For the networking plugin, many of the checks are handled by
SoSPredicates, and so those command collections now use Predicates
rather than being gated by individual `exec_cmd()` calls.

Resolves: #1807

Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>
Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

sos/plugins/block.py
@@ -19,22 +19,6 @@ class Block(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
     verify_packages = ('util-linux',)
     files = ('/sys/block',)
 
-    def get_luks_devices(self, lsblk_file):
-        out = []
-        try:
-            lsblk_out = open(lsblk_file).read()
-        except IOError:
-            return out
-        for line in lsblk_out.splitlines():
-            # find in output lines like
-            # sda2      crypto_LUKS    <uuid>
-            # loop0     crypto_LUKS    <uuid>
-            # and separate device name - it will be the 1st string on the line
-            if 'crypto_LUKS' in line:
-                dev = line.split()[0]
-                out.append(dev)
-        return out
-
     def setup(self):
         self.add_cmd_output([
             "lsblk",
@@ -69,10 +53,12 @@ class Block(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
                     "fdisk -l %s" % disk_path
                 ])
 
-        lsblk_file = self.get_cmd_output_now("lsblk -f -a -l")
+        lsblk = self.exec_cmd("lsblk -f -a -l")
         # for LUKS devices, collect cryptsetup luksDump
-        if lsblk_file:
-            for dev in self.get_luks_devices(lsblk_file):
-                self.add_cmd_output('cryptsetup luksDump /dev/%s' % dev)
+        if lsblk['status'] == 0:
+            for line in lsblk['output'].splitlines():
+                if 'crypto LUKS' in line:
+                    dev = line.split()[0]
+                    self.add_cmd_output('cryptsetup luksDump /dev/%s' % dev)
 
 # vim: set et ts=4 sw=4 :

sos/plugins/networking.py
@@ -26,24 +26,6 @@ class Networking(Plugin):
     # switch to enable netstat "wide" (non-truncated) output mode
     ns_wide = "-W"
 
-    def get_ip_netns(self, ip_netns_file):
-        """Returns a list for which items are namespaces in the output of
-        ip netns stored in the ip_netns_file.
-        """
-        out = []
-        try:
-            ip_netns_out = open(ip_netns_file).read()
-        except IOError:
-            return out
-        for line in ip_netns_out.splitlines():
-            # If there's no namespaces, no need to continue
-            if line.startswith("Object \"netns\" is unknown") \
-               or line.isspace() \
-               or line[:1].isspace():
-                return out
-            out.append(line.partition(' ')[0])
-        return out
-
     def collect_iptable(self, tablename):
         """ When running the iptables command, it unfortunately auto-loads
         the modules before trying to get output.  Some people explicitly
@@ -51,25 +33,25 @@ class Networking(Plugin):
         the command.  If they aren't loaded, there can't possibly be any
         relevant rules in that table """
 
-        modname = "iptable_"+tablename
-        if self.check_ext_prog("grep -q %s /proc/modules" % modname):
-            cmd = "iptables -t "+tablename+" -nvL"
-            self.add_cmd_output(cmd)
+        modname = "iptable_" + tablename
+        cmd = "iptables -t " + tablename + " -nvL"
+        self.add_cmd_output(cmd, pred=SoSPredicate(self, kmods=[modname]))
 
     def collect_ip6table(self, tablename):
         """ Same as function above, but for ipv6 """
 
-        modname = "ip6table_"+tablename
-        if self.check_ext_prog("grep -q %s /proc/modules" % modname):
-            cmd = "ip6tables -t "+tablename+" -nvL"
-            self.add_cmd_output(cmd)
+        modname = "ip6table_" + tablename
+        cmd = "ip6tables -t " + tablename + " -nvL"
+        self.add_cmd_output(cmd, pred=SoSPredicate(self, kmods=[modname]))
 
     def collect_nftables(self):
         """ Collects nftables rulesets with 'nft' commands if the modules
         are present """
 
-        if self.check_ext_prog("grep -q nf_tables /proc/modules"):
-            self.add_cmd_output("nft list ruleset")
+        self.add_cmd_output(
+            "nft list ruleset",
+            pred=SoSPredicate(self, kmods=['nf_tables'])
+        )
 
     def setup(self):
         super(Networking, self).setup()
@@ -163,10 +145,15 @@ class Networking(Plugin):
         # When iptables is called it will load the modules
         # iptables and iptables_filter if they are not loaded.
         # The same goes for ipv6.
-        if self.check_ext_prog("grep -q iptable_filter /proc/modules"):
-            self.add_cmd_output("iptables -vnxL")
-        if self.check_ext_prog("grep -q ip6table_filter /proc/modules"):
-            self.add_cmd_output("ip6tables -vnxL")
+        self.add_cmd_output(
+            "iptables -vnxL",
+            pred=SoSPredicate(self, kmods=['iptable_filter'])
+        )
+
+        self.add_cmd_output(
+            "ip6tables -vnxL",
+            pred=SoSPredicate(self, kmods=['ip6table_filter'])
+        )
 
         # Get ethtool output for every device that does not exist in a
         # namespace.
@@ -207,10 +194,18 @@ class Networking(Plugin):
 
         # Capture additional data from namespaces; each command is run
         # per-namespace.
-        ip_netns_file = self.get_cmd_output_now("ip netns")
+        ip_netns = self.exec_cmd("ip netns")
         cmd_prefix = "ip netns exec "
-        if ip_netns_file:
-            for namespace in self.get_ip_netns(ip_netns_file):
+        if ip_netns['status'] == 0:
+            out_ns = []
+            for line in ip_netns['output'].splitlines():
+                # If there's no namespaces, no need to continue
+                if line.startswith("Object \"netns\" is unknown") \
+                        or line.isspace() \
+                        or line[:1].isspace():
+                    continue
+                out_ns.append(line.partition(' ')[0])
+            for namespace in out_ns:
                 ns_cmd_prefix = cmd_prefix + namespace + " "
                 self.add_cmd_output([
                     ns_cmd_prefix + "ip address show",
@@ -229,10 +224,11 @@ class Networking(Plugin):
 
             # Devices that exist in a namespace use less ethtool
             # parameters. Run this per namespace.
-            for namespace in self.get_ip_netns(ip_netns_file):
+            for namespace in out_ns:
                 ns_cmd_prefix = cmd_prefix + namespace + " "
-                netns_netdev_list = self.call_ext_prog(ns_cmd_prefix +
-                                                       "ls -1 /sys/class/net/")
+                netns_netdev_list = self.exec_cmd(
+                    ns_cmd_prefix + "ls -1 /sys/class/net/"
+                )
                 for eth in netns_netdev_list['output'].splitlines():
                     # skip 'bonding_masters' file created when loading the
                     # bonding module but the file does not correspond to

sos/plugins/ntp.py
@@ -26,11 +26,10 @@ class Ntp(Plugin):
         ])
         self.add_cmd_output([
             "ntptime",
-            "ntpq -pn",
-            "ntpq -c as"
+            "ntpq -pn"
         ])
 
-        ids = self.get_command_output('ntpq -c as')
+        ids = self.exec_cmd('ntpq -c as')
         if ids['status'] == 0:
             for asid in [i.split()[1] for i in ids['output'].splitlines()[3:]]:
                 self.add_cmd_output("ntpq -c 'rv %s'" % asid)

sos/plugins/openstack_keystone.py
@@ -48,7 +48,7 @@ class OpenStackKeystone(Plugin):
 
         # collect domain config directory, if specified
         # if not, collect default /etc/keystone/domains
-        self.domain_config_dir = self.get_cmd_output_now(
+        self.domain_config_dir = self.exec_cmd(
                 "crudini --get /etc/keystone/keystone.conf "
                 "identity domain_config_dir")
         if self.domain_config_dir is None or \

sos/plugins/virsh.py
@@ -43,9 +43,8 @@ class LibvirtClient(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
 
         # get network, pool and nwfilter elements
         for k in ['net', 'nwfilter', 'pool']:
-            self.add_cmd_output('%s %s-list' % (cmd, k))
-            k_list = self.get_command_output('%s %s-list' % (cmd, k))
-            if k_list and k_list['status'] == 0:
+            k_list = self.exec_cmd('%s %s-list' % (cmd, k))
+            if k_list['status'] == 0:
                 k_lines = k_list['output'].splitlines()
                 # the 'Name' column position changes between virsh cmds
                 pos = k_lines[0].split().index('Name')
@@ -55,8 +54,8 @@ class LibvirtClient(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
 
         # cycle through the VMs/domains list, ignore 2 header lines and latest
         # empty line, and dumpxml domain name in 2nd column
-        domains_output = self.get_command_output('%s list --all' % cmd)
-        if domains_output and domains_output['status'] == 0:
+        domains_output = self.exec_cmd('%s list --all' % cmd)
+        if domains_output['status'] == 0:
             domains_lines = domains_output['output'].splitlines()[2:]
             for domain in filter(lambda x: x, domains_lines):
                 d = domain.split()[1]

commit 1681b561c1ace748f95659c4173db9a33ddb52aa

[plugins] collect missing command outputs from e51d3e6
Commit e51d3e6 caused some commands are called but not further
stored in an sosreport archive. This fixes that regression.

Related to / extends: #1923

Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

sos/plugins/block.py
@@ -53,7 +53,7 @@ class Block(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
                     "fdisk -l %s" % disk_path
                 ])
 
-        lsblk = self.exec_cmd("lsblk -f -a -l")
+        lsblk = self.collect_cmd_output("lsblk -f -a -l")
         # for LUKS devices, collect cryptsetup luksDump
         if lsblk['status'] == 0:
             for line in lsblk['output'].splitlines():

sos/plugins/networking.py
@@ -197,7 +197,7 @@ class Networking(Plugin):
 
         # Capture additional data from namespaces; each command is run
         # per-namespace.
-        ip_netns = self.exec_cmd("ip netns")
+        ip_netns = self.collect_cmd_output("ip netns")
         cmd_prefix = "ip netns exec "
         if ip_netns['status'] == 0:
             out_ns = []

sos/plugins/ntp.py
@@ -29,7 +29,7 @@ class Ntp(Plugin):
             "ntpq -pn"
         ])
 
-        ids = self.exec_cmd('ntpq -c as')
+        ids = self.collect_cmd_output('ntpq -c as')
         if ids['status'] == 0:
             for asid in [i.split()[1] for i in ids['output'].splitlines()[3:]]:
                 self.add_cmd_output("ntpq -c 'rv %s'" % asid)

sos/plugins/openstack_keystone.py
@@ -48,7 +48,7 @@ class OpenStackKeystone(Plugin):
 
         # collect domain config directory, if specified
         # if not, collect default /etc/keystone/domains
-        exec_out = self.exec_cmd(
+        exec_out = self.collect_cmd_output(
                 "crudini --get /etc/keystone/keystone.conf "
                 "identity domain_config_dir")
         self.domain_config_dir = exec_out['output']

sos/plugins/virsh.py
@@ -43,7 +43,7 @@ class LibvirtClient(Plugin, RedHatPlugin, UbuntuPlugin, DebianPlugin):
 
         # get network, pool and nwfilter elements
         for k in ['net', 'nwfilter', 'pool']:
-            k_list = self.exec_cmd('%s %s-list' % (cmd, k))
+            k_list = self.collect_cmd_output('%s %s-list' % (cmd, k))
             if k_list['status'] == 0:
                 k_lines = k_list['output'].splitlines()
                 # the 'Name' column position changes between virsh cmds