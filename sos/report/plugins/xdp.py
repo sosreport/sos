af5597bfa41521e10f4ef5095f137b2ef5bcd661
Toke Høiland-Jørgensen <toke@redhat.com>
2020-05-20 15:05:03 +0200

    [xdp] Add XDP plugin
    This adds a plugin for gathering information about the loaded XDP programs
    on a system, using the xdp-loader tool in the xdp-tools package. This is
    complementary with the existing eBPF plugin, because this plugin supports
    getting status for multiple XDP programs on each interface as supported by
    libxdp.
    
    For now, this just prints the output of 'xdp-loader status' which will list
    any XDP programs attached. We don't currently support structured
    output (such as JSON) from xdp-tools, but should we add that in the future
    we can of course update this plugin.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ sos/report/plugins/xdp.py
@@ -0,0 +1,21 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, RedHatPlugin, DebianPlugin, \
+    UbuntuPlugin
+
+
+class Xdp(Plugin, RedHatPlugin, DebianPlugin, UbuntuPlugin):
+
+    short_desc = 'XDP program information'
+    plugin_name = 'xdp'
+    profiles = ('system', 'kernel', 'network')
+    packages = ('xdp-tools',)
+
+    def setup(self):
+        self.add_cmd_output('xdp-loader status')
