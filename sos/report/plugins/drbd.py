bbd7d5f72b38ebe4bf93722d8677490eb1bf6e8f
Jose Castillo <jcastillo@redhat.com>
2020-06-18 10:32:10 +0100

    [drbd] Add new plugin for DRBD
    This patch implements a new plugin
    to capture information about Distributed
    Replicated Block Device (DRBD) setups.
    
    Solves: #2102
    
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

---
+++ sos/report/plugins/drbd.py
@@ -0,0 +1,33 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, RedHatPlugin, UbuntuPlugin
+
+
+class drbd(Plugin, RedHatPlugin, UbuntuPlugin):
+
+    short_desc = 'Distributed Replicated Block Device (DRBD)'
+
+    plugin_name = 'drbd'
+    profiles = ('storage')
+    packages = ('drbd.*-utils',)
+
+    def setup(self):
+        self.add_cmd_output([
+            "drbd-overview",
+            "drbdadm dump-xml",
+            "drbdsetup status",
+            "drbdsetup show"
+        ])
+        self.add_copy_spec([
+            "/etc/drbd.conf",
+            "/etc/drbd.d/*",
+            "/proc/drbd"
+        ])
+
+# vim: set et ts=4 sw=4 :
