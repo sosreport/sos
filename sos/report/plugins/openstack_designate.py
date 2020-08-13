b84c41f7428fee8cac692ac2f9c8bf76dbe2958e
Michael Johnson <johnsomor@gmail.com>
2020-08-13 15:21:28 -0700

    [openstack_designate] Add list command output
    This patch adds the various "list" command output for OpenStack
    designate to the SOS report.
    
    Closes: #2150
    
    Signed-off-by: Michael Johnson <johnsomor@gmail.com>

--- sos/report/plugins/openstack_designate.py
+++ sos/report/plugins/openstack_designate.py
@@ -18,6 +18,18 @@ class OpenStackDesignate(Plugin):
 
     var_puppet_gen = "/var/lib/config-data/puppet-generated/designate"
 
+    subcmds = ['dns service list',
+               'dns quota list',
+               'ptr record list',
+               'tld list',
+               'tsigkey list --column name --column algorithm --column scope',
+               'zone blacklist list',
+               'zone export list',
+               'zone import list',
+               'zone list',
+               'zone transfer accept list',
+               'zone transfer request list']
+
     def setup(self):
         # configs
         self.add_copy_spec([
@@ -37,6 +49,20 @@ class OpenStackDesignate(Plugin):
                 "/var/log/designate/*.log",
                 "/var/log/containers/designate/*.log"
             ])
+
+        # commands
+        self.add_cmd_output(['openstack %s --all-projects' %
+                             sub for sub in subcmds])
+
+        # get recordsets for each zone
+        cmd = "openstack zone list -f value -c id"
+        ret = self.exec_cmd(cmd)
+        if ret['status'] == 0:
+            for zone in ret['output'].splitlines():
+                zone = zone.split()[0]
+                self.add_cmd_output(
+                    "openstack recordset list --all-projects %s" % zone,
+                    subdir='recordset')
 
     def postproc(self):
         protect_keys = [
