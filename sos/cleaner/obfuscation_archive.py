32189e0b9334ed337a0d4887318ac45b6f3b609f
Jake Hunsaker <jhunsake@redhat.com>
2020-09-21 13:12:23 -0400

    [cleaner] Make tarball regex raw strings
    Make the tarball regex strings raw strings, to satisfy a flake8 rule for
    escape sequences.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- sos/cleaner/obfuscation_archive.py
+++ sos/cleaner/obfuscation_archive.py
@@ -73,9 +73,9 @@ class SoSObfuscationArchive():
             'sys/kernel/debug',
             'sys/module',
             'var/log/.*dnf.*',
-            '.*\.tar$',  # TODO: support archive unpacking
+            r'.*\.tar$',  # TODO: support archive unpacking
             # Be explicit with these tar matches to avoid matching commands
-            '.*\.tar\.xz',
+            r'.*\.tar\.xz',
             '.*.gz'
         ]
 
