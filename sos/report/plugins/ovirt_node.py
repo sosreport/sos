f8d66a716d527c2c4c8a83d2824c5e8059de5eab
Lev Veyde <lveyde@redhat.com>
2021-01-12 18:24:25 +0200

    [ovirt_node] Updated the ovirt_node plugin to collect additional data
    The plugin now collects information from certificates
    that were generated by oVirt on the host(s)/node(s).
    
    Related: RHBZ#1845877
    
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

--- sos/report/plugins/ovirt_node.py
+++ sos/report/plugins/ovirt_node.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2021 Red Hat, Inc., Lev Veyde <lveyde@redhat.com>
 # Copyright (C) 2018 Red Hat, Inc.,
 # This file is part of the sos project: https://github.com/sosreport/sos
 #
@@ -31,11 +32,25 @@ class OvirtNode(Plugin, RedHatPlugin):
             '/tmp/imgbased.log',
         ])
 
+        certificates = [
+            '/etc/pki/vdsm/certs/cacert.pem',
+            '/etc/pki/vdsm/certs/vdsmcert.pem',
+            '/etc/pki/vdsm/libvirt-spice/ca-cert.pem',
+            '/etc/pki/vdsm/libvirt-spice/server-cert.pem',
+            '/etc/pki/vdsm/libvirt-vnc/ca-cert.pem',
+            '/etc/pki/vdsm/libvirt-vnc/server-cert.pem',
+        ]
+
         # Collect runtime info
         self.add_cmd_output([
             'imgbase layout',
             'nodectl --machine-readable check',
             'nodectl info',
+        ])
+
+        # Collect certificate info
+        self.add_cmd_output([
+            'openssl x509 -in %s -text -noout' % c for c in certificates
         ])
 
 
