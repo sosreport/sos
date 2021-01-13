1e36c312ca765aced444f229aeac78844942cfcf
Jake Hunsaker <jhunsake@redhat.com>
2021-01-13 17:03:08 -0500

    [tigervnc] Add new plugin
    Adds a new plugin for the TigerVNC plugin
    
    Closes: #2347
    Resolves: #2371
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ sos/report/plugins/tigervnc.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2021 Red Hat, Inc., Jake Hunsaker <jhunsake@redhat.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, RedHatPlugin
+
+
+class TigerVNC(Plugin, RedHatPlugin):
+
+    plugin_name = 'tigervnc'
+    packages = ('tigervnc-server',)
+
+    def setup(self):
+        self.add_copy_spec([
+            '/etc/tigervnc/vncserver-config-defaults',
+            '/etc/tigervnc/vncserver-config-mandatory',
+            '/etc/tigervnc/vncserver.users'
+        ])
+
+        self.add_cmd_output('vncserver -list')
+
+# vim: set et ts=4 sw=4 :
