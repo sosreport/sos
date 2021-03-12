b8b0417bd0a9af8881fa0d7271e9430b48403704
Jan Jansky <jjansky@redhat.com>
2021-03-12 16:42:13 +0100

    [filesys] never collect content of /proc/fs/panfs
    Backport #2262 to legacy-3.9.
    
    Related: #2262
    Resolves: #2447
    
    Signed-off-by: Jan Jansky <jjansky@redhat.com>

--- sos/plugins/filesys.py
+++ sos/plugins/filesys.py
@@ -42,6 +42,8 @@ class Filesys(Plugin, DebianPlugin, UbuntuPlugin):
             "lslocks"
         ])
 
+        self.add_forbidden_path('/proc/fs/panfs')
+
         if self.get_option('lsof'):
             self.add_cmd_output("lsof -b +M -n -l -P", root_symlink="lsof")
 
