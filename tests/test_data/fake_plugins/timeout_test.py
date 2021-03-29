5e3eb985367c0d945b45a2e07b01e3fc37e7e18e
Jake Hunsaker <jhunsake@redhat.com>
2021-03-29 11:04:46 -0400

    [tests] Add a StageTwo timeout test
    Adds a test to ensure our timeout control is working properly.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ tests/test_data/fake_plugins/timeout_test.py
@@ -0,0 +1,21 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, IndependentPlugin
+
+
+class TimeoutTest(Plugin, IndependentPlugin):
+
+    plugin_name = 'timeout_test'
+    short_desc = 'Tests timeout functionality in test suite'
+
+
+    def setup(self):
+        self.add_cmd_output('sleep 15')
+        self.add_cmd_output('echo I slept great', suggest_filename='echo_good')
+        self.add_cmd_output('sleep 30', timeout=10)
