05b456451be1340cf94efacb37fae5130f60f5e1
Jake Hunsaker <jhunsake@redhat.com>
2021-03-29 13:20:56 -0400

    [tests] Add plugin tests for basic functionality
    Adds several plugin tests for plugins that can be expected to run on all
    distributions, in particular these tests are including mostly Stage Two
    tests.
    
    As such, these tests are only recommended to be run on test machines.
    
    Related: #2431
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

---
+++ tests/report_tests/plugin_tests/sos_extras.py
@@ -0,0 +1,32 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos_tests import StageTwoReportTest
+
+
+class SosExtrasPluginTest(StageTwoReportTest):
+    """Ensure that the sos_extras plugin is properly executing command and
+    file collections as defined in the sos_extras config file
+
+    :avocado: tags=stagetwo
+    """
+
+    files = ['/etc/sos/extras.d/sos_testing.conf']
+    # rather than only enabling this plugin, make sure the enablement trigger
+    # is working
+    sos_cmd = '-n logs,networking,devicemapper,filesys,systemd'
+
+    def test_extras_enabled(self):
+        self.assertPluginIncluded('sos_extras')
+
+    def test_setup_message_displayed(self):
+        self.assertOutputContains('Collecting data from extras file /etc/sos/extras.d/sos_testing.conf')
+
+    def test_extras_config_parsed(self):
+        self.assertFileCollected('/etc/fstab')
+        self.assertFileCollected('sos_commands/sos_extras/sos_testing.conf/echo_sos_test')
---
+++ tests/report_tests/plugin_tests/sudo.py
@@ -0,0 +1,36 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos_tests import StageOneReportTest, StageTwoReportTest
+
+
+class SudoPluginTest(StageOneReportTest):
+    """Basic sanity check to make sure ocmmon config files are collected
+
+    :avocado: tags=stageone
+    """
+
+    sos_cmd = '-o sudo'
+
+    def test_sudo_conf_collected(self):
+        self.assertFileCollected('/etc/sudo.conf')
+        self.assertFileCollected('/etc/sudoers')
+
+
+class SudoLdapScrubbedTest(StageTwoReportTest):
+    """Ensure that sudo conf is picked up and properly scrubbed
+
+    :avocado: tags=stagetwo
+    """
+
+    sos_cmd = '-o sudo'
+    files = ['/etc/sudo-ldap.conf']
+
+    def test_bindpw_scrubbed(self):
+        self.assertFileNotHasContent('/etc/sudo-ldap.conf', 'sostestpassword')
+
