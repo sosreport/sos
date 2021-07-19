bdbed46d062a6e5be7805f8588de70389dc5dd74
liugang <liuganga@uniontech.com>
2021-07-19 15:14:10 +0800

    [os supprt] add UnionTech OS support
    Signed-off-by: liugang <liuganga@uniontech.com>

---
+++ sos/policies/distros/UnionTech.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+from sos.plugins import RedHatPlugin
+from sos.policies.redhat import RedHatPolicy, OS_RELEASE
+import os
+
+class UnionTechPolicy(RedHatPolicy):
+
+    distro = "UnionTech"
+    vendor = "the UnionTech Project"
+    vendor_url = "https://www.chinauos.com/"
+
+    def __init__(self, sysroot=None):
+        super(UnionTechPolicy, self).__init__(sysroot=sysroot)
+
+    @classmethod
+    def check(cls):
+        if not os.path.exists(OS_RELEASE):
+            return False
+
+        with open(OS_RELEASE, 'r') as f:
+            for line in f:
+                if line.startswith('NAME'):
+                    if 'UnionTech OS Server 20' in line:
+                        return True
+        return False
+
+
+
