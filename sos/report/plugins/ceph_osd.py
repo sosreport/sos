22754fd1275c2c1597a953c02121b1fdf57dddd0
Nikhil Kshirsagar <nkshirsagar@gmail.com>
2021-10-28 14:37:07 +0530

    [ceph_osd] Add more data collection
    Enhance the ceph_osd plugin to collect more data specific to OSD nodes.
    
    Related: #1945
    Resolves: #2735
    
    Signed-off-by: Nikhil Kshirsagar <nkshirsagar@gmail.com>

--- sos/report/plugins/ceph_osd.py
+++ sos/report/plugins/ceph_osd.py
@@ -39,8 +39,58 @@ class CephOSD(Plugin, RedHatPlugin, UbuntuPlugin):
 
         self.add_cmd_output([
             "ceph-disk list",
-            "ceph-volume lvm list",
+            "ceph-volume lvm list"
         ])
+
+        ceph_cmds = [
+           "bluestore bluefs available",
+           "config diff",
+           "config show",
+           "dump_blacklist",
+           "dump_blocked_ops",
+           "dump_historic_ops_by_duration",
+           "dump_historic_slow_ops",
+           "dump_mempools",
+           "dump_ops_in_flight",
+           "dump_op_pq_state",
+           "dump_osd_network",
+           "dump_reservations",
+           "dump_watchers",
+           "log dump",
+           "perf dump",
+           "perf histogram dump",
+           "objecter_requests",
+           "ops",
+           "status",
+           "version",
+        ]
+
+        osd_ids = []
+        # Get the ceph user processes
+        out = self.exec_cmd('ps -u ceph -o args')
+
+        if out['status'] == 0:
+            # Extract the OSD ids from valid output lines
+            for procs in out['output'].splitlines():
+                proc = procs.split()
+                if len(proc) < 6:
+                    continue
+                if proc[4] == '--id' and proc[5].isdigit():
+                    osd_ids.append("osd.%s" % proc[5])
+
+        containers_list = self.get_all_containers_by_regex("ceph-osd*")
+
+        if containers_list:
+            self.add_cmd_output([
+                self.fmt_container_cmd(
+                    containers_list[0][1], "ceph daemon %s %s"
+                    % (osid, cmd)) for osid in osd_ids for cmd in ceph_cmds
+            ])
+        else:
+            self.add_cmd_output([
+                "ceph daemon %s %s" % (
+                    osid, cmd) for osid in osd_ids for cmd in ceph_cmds
+            ])
 
         self.add_forbidden_path([
             "/etc/ceph/*keyring*",
