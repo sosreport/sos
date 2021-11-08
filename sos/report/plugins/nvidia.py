f2cc67750f55a71edff0c527a1bfc14fde8132c3
Mamatha Inamdar <mamatha4@linux.vnet.ibm.com>
2021-11-08 10:50:03 +0530

    [nvidia]:Patch to update nvidia plugin for GPU info
    This patch is to update nvidia plugin to collect
    logs for Nvidia GPUs
    
    Signed-off-by: Mamatha Inamdar <mamatha4@linux.vnet.ibm.com>
    Reported-by: Borislav Stoymirski <borislav.stoymirski@bg.ibm.com>
    Reported-by: Yesenia Jimenez <yesenia@us.ibm.com>

--- sos/report/plugins/nvidia.py
+++ sos/report/plugins/nvidia.py
@@ -23,13 +23,24 @@ class Nvidia(Plugin, IndependentPlugin):
             '--list-gpus',
             '-q -d PERFORMANCE',
             '-q -d SUPPORTED_CLOCKS',
-            '-q -d PAGE_RETIREMENT'
+            '-q -d PAGE_RETIREMENT',
+            '-q',
+            '-q -d ECC',
+            'nvlink -s',
+            'nvlink -e'
         ]
 
         self.add_cmd_output(["nvidia-smi %s" % cmd for cmd in subcmds])
 
         query = ('gpu_name,gpu_bus_id,vbios_version,temperature.gpu,'
-                 'utilization.gpu,memory.total,memory.free,memory.used')
+                 'utilization.gpu,memory.total,memory.free,memory.used,'
+                 'clocks.applications.graphics,clocks.applications.memory')
+        querypages = ('timestamp,gpu_bus_id,gpu_serial,gpu_uuid,'
+                      'retired_pages.address,retired_pages.cause')
         self.add_cmd_output("nvidia-smi --query-gpu=%s --format=csv" % query)
+        self.add_cmd_output(
+            "nvidia-smi --query-retired-pages=%s --format=csv" % querypages
+        )
+        self.add_journal(boot=0, identifier='nvidia-persistenced')
 
 # vim: set et ts=4 sw=4 :
