8aec430fd27785aa85a6e677151a2bb76da244dc
Jake Hunsaker <jhunsake@redhat.com>
2022-04-20 11:19:30 -0400

    [collect] Add cluster profile for RHOSP
    Adds a cluster profile for Red Hat OpenStack Platform to identify
    controller and (optionally) compute nodes to collect sos reports from.
    
    Note that this adds a dependency on pyyaml to sos. This should be
    considered a weak dependency by downstreams. As such, it is added as a
    'Recommends' in `sos.spec`. pip does not have the concept of 'weak
    dependencies', and so is added as a regular requirement in `setup.py`.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     ],
     cmdclass=cmdclass,
     command_options=command_options,
-    requires=['pexpect', 'python_magic']
+    requires=['pexpect', 'python_magic', 'pyyaml']
     )
 
 
