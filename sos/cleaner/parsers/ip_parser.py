5374649a4b89e8cb4dfacb690bac707e0bc4c21f
Jake Hunsaker <jhunsake@redhat.com>
2022-06-28 17:46:49 -0400

    [clean,IP Parser] Skip obvious version files for the IP parser
    For files that can be considered obvious version files - those that end
    specifically with either `version` or `version.txt` - skip processing
    via the IP parser, as this may lead to improper obfuscation of certain
    version strings. This is also applied to files ending in `release`.
    
    Closes: #2962
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- sos/cleaner/parsers/ip_parser.py
+++ sos/cleaner/parsers/ip_parser.py
@@ -35,10 +35,10 @@ class SoSIPParser(SoSCleanerParser):
         'sos_commands/rpm',
         'sos_commands/yum/.*list.*',
         'sos_commands/snappy/snap_list_--all',
-        'sos_commands/snappy/snap_--version',
         'sos_commands/vulkan/vulkaninfo',
         'var/log/.*dnf.*',
-        'var/log/.*packag.*'  # get 'packages' and 'packaging' logs
+        'var/log/.*packag.*',  # get 'packages' and 'packaging' logs
+        '.*(version|release)(\\.txt)?$',  # obvious version files
     ]
 
     map_file_key = 'ip_map'
