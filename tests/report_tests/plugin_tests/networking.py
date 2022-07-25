ef131e2a22a1c9ac39efb60dc65efc9667aded8c
Jake Hunsaker <jhunsake@redhat.com>
2022-07-25 10:40:37 -0400

    [tests] Update networking test in suite
    Adds a new test to the networking plugin test to ensure we iterate
    correctly over network devices.

--- tests/report_tests/plugin_tests/networking.py
+++ tests/report_tests/plugin_tests/networking.py
@@ -6,6 +6,7 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
+import os
 
 from sos_tests import StageOneReportTest
 
@@ -29,3 +30,9 @@ class NetworkingPluginTest(StageOneReportTest):
     def test_forbidden_globs_skipped(self):
         self.assertFileGlobNotInArchive('/proc/net/rpc/*/channel')
         self.assertFileGlobNotInArchive('/proc/net/rpc/*/flush')
+
+    def test_netdevs_properly_iterated(self):
+        for dev in os.listdir('/sys/class/net'):
+            self.assertFileGlobInArchive(
+                "sos_commands/networking/ethtool_*_%s" % dev
+            )
