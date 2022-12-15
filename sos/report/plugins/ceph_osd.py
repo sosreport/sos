b9dcff190e330c8a708508a4dfbd2c4740a34056
Jake Hunsaker <jhunsake@redhat.com>
2022-12-15 17:48:17 -0500

    [ceph_osd] Update plugin for newer versions of ceph
    Newer versions of Ceph, such as for RHCS 5, make some changes to the
    container names as well as how to leverage `ceph daemon` commands.
    Update the plugin to reflect this, and use the available admin sockets
    on the host to capture that output, rather than always running it inside
    the osd container.
    
    Further, account for the use of an fsid in directory paths for newer ceph.
    
    Signed-off-by: Jake Hunsaker <jhunsake@redhat.com>

--- sos/report/plugins/ceph_osd.py
+++ sos/report/plugins/ceph_osd.py
@@ -6,29 +6,66 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
+import os
+
 from sos.report.plugins import Plugin, RedHatPlugin, UbuntuPlugin
 
 
 class CephOSD(Plugin, RedHatPlugin, UbuntuPlugin):
+    """
+    This plugin is for capturing information from Ceph OSD nodes. While the
+    majority of this plugin should be version agnotics, several collections are
+    dependent upon the version of Ceph installed. Versions that correlate to
+    RHCS 4 or RHCS 5 are explicitly handled for differences such as those
+    pertaining to log locations on the host filesystem.
+
+    Note that while this plugin will activate based on the presence of Ceph
+    containers, commands are run directly on the host as those containers are
+    often not configured to successfully run the `ceph` commands collected by
+    this plugin. These commands are majorily `ceph daemon` commands that will
+    reference discovered admin sockets under /var/run/ceph.
+    """
 
     short_desc = 'CEPH osd'
 
     plugin_name = 'ceph_osd'
     profiles = ('storage', 'virt', 'container')
-    containers = ('ceph-(.*)?osd.*',)
-    files = ('/var/lib/ceph/osd/',)
+    containers = ('ceph-(.*-)?osd.*',)
+    files = ('/var/lib/ceph/osd/', '/var/lib/ceph/*/osd*')
 
     def setup(self):
+
+        self.ceph_version = self.get_ceph_version()
+
+        logdir = '/var/log/ceph'
+        libdir = '/var/lib/ceph'
+        rundir = '/run/ceph'
+
+        if self.ceph_version >= 16:
+            logdir += '/*'
+            libdir += '/*'
+            rundir += '/*'
+
         self.add_file_tags({
-            '/var/log/ceph/ceph-osd.*.log': 'ceph_osd_log',
+            f"{logdir}/ceph-(.*-)?osd.*.log": 'ceph_osd_log',
         })
+
+        self.add_forbidden_path([
+            "/etc/ceph/*keyring*",
+            f"{libdir}/*keyring*",
+            f"{libdir}/**/*keyring*",
+            # Excludes temporary ceph-osd mount location like
+            # /var/lib/ceph/tmp/mnt.XXXX from sos collection.
+            f"{libdir}/tmp/*mnt*",
+            "/etc/ceph/*bindpass*"
+        ])
 
         # Only collect OSD specific files
         self.add_copy_spec([
-            "/run/ceph/ceph-osd*",
-            "/var/lib/ceph/osd/*/kv_backend",
-            "/var/log/ceph/ceph-osd*.log",
-            "/var/log/ceph/ceph-volume*.log",
+            f"{rundir}/ceph-osd*",
+            f"{libdir}/**/kv_backend",
+            f"{logdir}/ceph-osd*.log",
+            f"{logdir}/ceph-volume*.log",
         ])
 
         self.add_cmd_output([
@@ -36,7 +73,7 @@ class CephOSD(Plugin, RedHatPlugin, UbuntuPlugin):
             "ceph-volume lvm list"
         ])
 
-        ceph_cmds = [
+        cmds = [
            "bluestore bluefs available",
            "config diff",
            "config show",
@@ -59,41 +96,33 @@ class CephOSD(Plugin, RedHatPlugin, UbuntuPlugin):
            "version",
         ]
 
-        osd_ids = []
-        # Get the ceph user processes
-        out = self.exec_cmd('ps -u ceph -o args')
-
-        if out['status'] == 0:
-            # Extract the OSD ids from valid output lines
-            for procs in out['output'].splitlines():
-                proc = procs.split()
-                # Locate the '--id' value
-                if proc and proc[0].endswith("ceph-osd"):
-                    try:
-                        id_index = proc.index("--id")
-                        osd_ids.append("osd.%s" % proc[id_index+1])
-                    except (IndexError, ValueError):
-                        self.log_warn("could not find ceph-osd id: %s", procs)
-
-        try:
-            cname = self.get_all_containers_by_regex("ceph-osd*")[0][1]
-        except Exception:
-            cname = None
-
         self.add_cmd_output(
-            ["ceph daemon %s %s" % (i, c) for i in osd_ids for c in ceph_cmds],
-            container=cname
+            [f"ceph daemon {i} {c}" for i in self.get_socks() for c in cmds]
         )
 
-        self.add_forbidden_path([
-            "/etc/ceph/*keyring*",
-            "/var/lib/ceph/*keyring*",
-            "/var/lib/ceph/*/*keyring*",
-            "/var/lib/ceph/*/*/*keyring*",
-            # Excludes temporary ceph-osd mount location like
-            # /var/lib/ceph/tmp/mnt.XXXX from sos collection.
-            "/var/lib/ceph/tmp/*mnt*",
-            "/etc/ceph/*bindpass*"
-        ])
+    def get_socks(self):
+        """
+        Find any available admin sockets under /var/run/ceph (or subdirs for
+        later versions of Ceph) which can be used for ceph daemon commands
+        """
+        ceph_sockets = []
+        for rdir, dirs, files in os.walk('/var/run/ceph/'):
+            for file in files:
+                if file.endswith('.asok'):
+                    ceph_sockets.append(self.path_join(rdir, file))
+        return ceph_sockets
+
+    def get_ceph_version(self):
+        ver = self.exec_cmd('ceph --version')
+        if ver['status'] == 0:
+            try:
+                _ver = ver['output'].split()[2]
+                return int(_ver.split('.')[0])
+            except Exception as err:
+                self._log_debug(f"Could not determine ceph version: {err}")
+        self._log_error(
+            'Failed to find ceph version, command collection will be limited'
+        )
+        return 0
 
 # vim: set et ts=4 sw=4 :
