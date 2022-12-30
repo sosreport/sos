11763c36ced8dd07d7586d59255e8d74754efe4d
Nobuto Murata <nobuto.murata@canonical.com>
2022-12-31 00:48:39 +0900

    [ceph] add iSCSI gateway
    Along with existing Ceph plugins, Ceph iSCSI gateway use case requires
    additional config and log files to be analyzed.
    https://docs.ceph.com/en/latest/rbd/iscsi-overview/
    
    Closes: #3098
    Related-Bug: https://bugs.launchpad.net/ubuntu/+source/sosreport/+bug/2000672
    Signed-off-by: Nobuto Murata <nobuto.murata@canonical.com>

---
+++ sos/report/plugins/ceph_iscsi.py
@@ -0,0 +1,36 @@
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.report.plugins import Plugin, RedHatPlugin, UbuntuPlugin
+
+
+class CephISCSI(Plugin, RedHatPlugin, UbuntuPlugin):
+
+    short_desc = "CEPH iSCSI"
+
+    plugin_name = "ceph_iscsi"
+    profiles = ("storage", "virt", "container")
+    packages = ("ceph-iscsi",)
+    services = ("rbd-target-api", "rbd-target-gw")
+    containers = ("rbd-target-api.*", "rbd-target-gw.*")
+
+    def setup(self):
+        self.add_copy_spec([
+            "/etc/tcmu/tcmu.conf",
+            "/var/log/**/ceph-client.*.log",
+            "/var/log/**/rbd-target-api.log",
+            "/var/log/**/rbd-target-gw.log",
+            "/var/log/tcmu-runner.log"
+        ])
+
+        self.add_cmd_output([
+            "gwcli info",
+            "gwcli ls"
+        ])
+
+# vim: set et ts=4 sw=4 :
