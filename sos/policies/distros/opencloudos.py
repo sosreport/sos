aa2d6b4cf0ad435e7b6c0c383d6c0a6222d4501e
zoedong <zoedong@tencent.com>
2023-01-05 14:44:33 +0800

    [policies] Add policy for OpenCloudOS
    Signed-off-by: zoedong <zoedong@tencent.com>

---
+++ sos/policies/distros/opencloudos.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2022 Tencent., ZoeDong <zoedong@tencent.com>
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.policies.distros.redhat import RedHatPolicy, OS_RELEASE
+import os
+
+
+class OpenCloudOSPolicy(RedHatPolicy):
+    distro = "OpenCloudOS Stream"
+    vendor = "OpenCloudOS"
+    vendor_urls = [('Distribution Website', 'https://www.opencloudos.org/')]
+
+    def __init__(self, sysroot=None, init=None, probe_runtime=True,
+                 remote_exec=None):
+        super(OpenCloudOSPolicy, self).__init__(sysroot=sysroot, init=init,
+                                                probe_runtime=probe_runtime,
+                                                remote_exec=remote_exec)
+
+    @classmethod
+    def check(cls, remote=''):
+
+        if remote:
+            return cls.distro in remote
+
+        if not os.path.exists(OS_RELEASE):
+            return False
+
+        with open(OS_RELEASE, 'r') as f:
+            for line in f:
+                if line.startswith('NAME'):
+                    if 'OpenCloudOS Stream' in line:
+                        return True
+
+        return False
+
+# vim: set et ts=4 sw=4 :
