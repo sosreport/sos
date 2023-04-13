6f023937bd3d660c8bc68505ec83d015943e7a22
Rafael Lopez <rafael.lopez@canonical.com>
2023-04-13 08:12:37 +0200

    [zfs] add collection of files from /proc/spl
    Add collection of files under /proc/spl which contain useful
    statistics and data for debugging and analysing zfs.
    
    Resolves: #3194
    
    Signed-off-by: Rafael Lopez <rafael.lopez@canonical.com>
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/report/plugins/zfs.py
+++ sos/report/plugins/zfs.py
@@ -28,6 +28,28 @@ class Zfs(Plugin, IndependentPlugin):
             "zpool status -vx"
         ])
 
+        self.add_copy_spec([
+            "/proc/spl/kmem/slab",
+            "/proc/spl/kstat/zfs/fm",
+            "/proc/spl/kstat/zfs/zil",
+            "/proc/spl/kstat/zfs/dbufs",
+            "/proc/spl/kstat/zfs/dbgmsg",
+            "/proc/spl/kstat/zfs/dmu_tx",
+            "/proc/spl/kstat/zfs/abdstats",
+            "/proc/spl/kstat/zfs/arcstats",
+            "/proc/spl/kstat/zfs/dbufstats",
+            "/proc/spl/kstat/zfs/dnodestats",
+            "/proc/spl/kstat/zfs/xuio_stats",
+            "/proc/spl/kstat/zfs/zfetchstats",
+            "/proc/spl/kstat/zfs/import_progress",
+            "/proc/spl/kstat/zfs/fletcher_4_bench",
+            "/proc/spl/kstat/zfs/vdev_cache_stats",
+            "/proc/spl/kstat/zfs/vdev_raidz_bench",
+            "/proc/spl/kstat/zfs/vdev_mirror_stats",
+            "/proc/spl/taskq",
+            "/proc/spl/taskq-all",
+        ])
+
         zpools = self.collect_cmd_output("zpool list -H -o name")
         if zpools['status'] == 0:
             zpools_list = zpools['output'].splitlines()
