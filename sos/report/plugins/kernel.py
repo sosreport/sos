cc8d97cc133c74b80c6f304ea68b5828457aa0d3
Pavel Moravec <pmoravec@redhat.com>
2023-06-15 08:49:55 +0200

    [kernel] verify also kernel-redhat on RedHatPolicy
    RedHat distributions can deliver kernel in RPM named either 'kernel' or
    'kernel-redhat', in which case we must verify both package names.
    
    Resolves: #3274
    
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- sos/report/plugins/kernel.py
+++ sos/report/plugins/kernel.py
@@ -7,6 +7,7 @@
 # See the LICENSE file in the source distribution for further information.
 
 from sos.report.plugins import Plugin, IndependentPlugin, PluginOpt
+from sos.policies.distros.redhat import RedHatPolicy
 import glob
 
 
@@ -42,6 +43,11 @@ class Kernel(Plugin, IndependentPlugin):
     ]
 
     def setup(self):
+        # RedHat distributions can deliver kernel in RPM named either 'kernel'
+        # or 'kernel-redhat', so we must verify both
+        if isinstance(self.policy, RedHatPolicy):
+            self.verify_packages = ('kernel$', 'kernel-redhat$')
+
         # compat
         self.add_cmd_output("uname -a", root_symlink="uname", tags="uname")
         self.add_cmd_output("lsmod", root_symlink="lsmod", tags="lsmod")
