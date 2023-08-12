c4e81d7b06547dc9383978cb93e0e60b8c4dd4a7
Jose Castillo <jcastillo@redhat.com>
2023-08-12 16:43:04 +0100

    [flake 8] Fix two new style alerts
    Two new alerts:
    
    sos/report/plugins/release.py:9:52: E231 missing
    whitespace after ','
    
    sos/report/plugins/__init__.py:456:21: E721 do not
    compare types, for exact checks use `is` / `is not`,
    for instance checks use `isinstance()
    
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

--- sos/report/plugins/__init__.py
+++ sos/report/plugins/__init__.py
@@ -453,7 +453,7 @@ class PluginOpt():
         if type('') in self.val_type:
             self.value = str(val)
             return
-        if not any([type(val) == _t for _t in self.val_type]):
+        if not any([type(val) is _t for _t in self.val_type]):
             valid = []
             for t in self.val_type:
                 if t is None:
--- sos/report/plugins/release.py
+++ sos/report/plugins/release.py
@@ -6,7 +6,7 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
-from sos.report.plugins import Plugin, RedHatPlugin,\
+from sos.report.plugins import Plugin, RedHatPlugin, \
     DebianPlugin, UbuntuPlugin, CosPlugin
 
 
