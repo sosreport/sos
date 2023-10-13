0e464b70f0a85b685631e77a95e70bc955fdd7a7
Jose Castillo <jcastillo@redhat.com>
2023-10-13 16:06:31 +0100

    [flatpak] Add flatpak pkg manager
    Related: RH SUPDEV-136
    
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

---
+++ sos/policies/package_managers/flatpak.py
@@ -0,0 +1,30 @@
+# Copyright 2023 Red Hat, Inc. Jose Castillo <jcastillo@redhat.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.policies.package_managers import PackageManager
+
+
+class FlatpakPackageManager(PackageManager):
+    """Package Manager for Flatpak distributions
+    """
+
+    query_command = 'flatpak list --columns=name,version'
+    query_path_command = ''
+    files_command = ''
+    verify_command = ''
+    verify_filter = ''
+
+    def _parse_pkg_list(self, pkg_list):
+        for line in pkg_list.splitlines():
+            pkg = line.split()
+            name, version = pkg[0], pkg[1]
+            yield (name, version, None)
+
+# vim: set et ts=4 sw=4 :
