a3ecda3527c7b2211950c9509f2b8f7b12a8846a
Eduard Abdullin <eabdullin@almalinux.org>
2024-07-04 12:22:45 +0300

    [policies] Add CloudLinux policy
    Signed-off-by: Eduard Abdullin <eabdullin@almalinux.org>

---
+++ sos/policies/distros/cloudlinux.py
@@ -0,0 +1,46 @@
+# Copyright (C) Eduard Abdullin <eabdullin@cloudlinux.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+import os
+from sos.policies.distros.redhat import RedHatPolicy, OS_RELEASE
+
+
+class CloudLinuxPolicy(RedHatPolicy):
+    distro = "CloudLinux"
+    vendor = "CloudLinux"
+    vendor_urls = [
+        ('Distribution Website', 'https://www.cloudlinux.com/'),
+        ('Commercial Support', 'https://www.cloudlinux.com/')
+    ]
+
+    def __init__(self, sysroot=None, init=None, probe_runtime=True,
+                 remote_exec=None):
+        super().__init__(sysroot=sysroot, init=init,
+                         probe_runtime=probe_runtime,
+                         remote_exec=remote_exec)
+
+    @classmethod
+    def check(cls, remote=''):
+        if remote:
+            return cls.distro in remote
+
+        if not os.path.isfile('/etc/cloudlinux-release'):
+            return False
+
+        if os.path.exists(OS_RELEASE):
+            with open(OS_RELEASE, 'r') as f:
+                for line in f:
+                    if line.startswith('NAME'):
+                        if 'CloudLinux' in line:
+                            return True
+
+        return False
+
+# vim: set et ts=4 sw=4 :
