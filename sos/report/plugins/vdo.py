23c75360f77776d3220b6db4a3028d88a4eb3de6
Jose Castillo <jcastillo@redhat.com>
2024-07-16 21:50:30 +0100

    [vdo] Capture more information about vdo volumes
    Capture more information about vdo volumes
    
    Related: VDO-5797
    
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

--- sos/report/plugins/vdo.py
+++ sos/report/plugins/vdo.py
@@ -28,6 +28,18 @@ class Vdo(Plugin, RedHatPlugin):
         vdos = self.collect_cmd_output('vdo list --all')
         for vdo in vdos['output'].splitlines():
             self.add_cmd_output(f"vdo status -n {vdo}")
-        self.add_cmd_output('vdostats --human-readable')
+        self.add_cmd_output([
+            'vdostats --human-readable',
+            'vdostats --verbose',
+        ])
+        vdo_cols = 'vdo_deduplication,vdo_index_state'
+        lvs_opts = '-a -o +'
+        lvm_vdos = self.collect_cmd_output(f"lvs {lvs_opts}{vdo_cols}")
+        for vdo in lvm_vdos['output'].splitlines():
+            # we can find the pool and pool data maps in the output
+            # of lvs, in the column Volume type, marked as either 'd' or 'D'
+            if vdo.split()[2].startswith("D"):
+                vdo_path = f"{vdo.split()[1]}-{vdo.split()[0].strip('[]')}"
+                self.add_cmd_output(f"vdodumpconfig /dev/mapper/{vdo_path}")
 
 # vim set et ts=4 sw=4 :
