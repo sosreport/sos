0eb082af606a8561569e9f3f05d94fa3947d5d62
Ponnuvel Palaniyappan <pponnuvel@gmail.com>
2024-08-08 13:13:35 +0100

    [pylint] Fix arguments-renamed and arguments-differ
    Signed-off-by: Ponnuvel Palaniyappan <pponnuvel@gmail.com>

--- sos/cleaner/mappings/username_map.py
+++ sos/cleaner/mappings/username_map.py
@@ -24,11 +24,11 @@ class SoSUsernameMap(SoSMap):
     match_full_words_only = True
     name_count = 0
 
-    def sanitize_item(self, username):
+    def sanitize_item(self, item):
         """Obfuscate a new username not currently found in the map
         """
         ob_name = f"obfuscateduser{self.name_count}"
         self.name_count += 1
         if ob_name in self.dataset.values():
-            return self.sanitize_item(username.lower())
+            return self.sanitize_item(item.lower())
         return ob_name
