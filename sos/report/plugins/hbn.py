f07278f602e16d954e52785f80f426ee2e982996
Hareesh Puthalath <hareeshp@nvidia.com>
2024-10-23 23:21:51 +0200

    [HBN] Add additional hbn related commands and log files.
    Signed-off-by: Hareesh Puthalath <hareeshp@nvidia.com>
    Signed-off-by: Michael Filanov <mfilanov@nvidia.com>

--- sos/report/plugins/hbn.py
+++ sos/report/plugins/hbn.py
@@ -12,7 +12,7 @@ from sos.report.plugins import Plugin, IndependentPlugin
 
 class HBN(Plugin, IndependentPlugin):
 
-    short_desc = 'HBN (Host Based Network)'
+    short_desc = 'HBN (Host Based Networking)'
     plugin_name = "hbn"
     packages = ('hbn-repo',)
 
@@ -21,4 +21,20 @@ class HBN(Plugin, IndependentPlugin):
             '/etc/mellanox',
             '/var/log/doca/hbn',
             '/var/lib/hbn/etc/cumulus',
+            '/var/log/sfc-install.log',
+            '/usr/lib/udev',            # HBN udev rules
+            '/tmp/sf_devices',          # udev renamed sf devices
+            '/tmp/sfr_devices',         # udev renamed sf representor devices
+            '/tmp/sfc-activated',
+            '/tmp/.BR_*',               # interim files for updating hbn.conf
+            '/tmp/.ENABLE_*',
+            '/tmp/.LINK_*',
+        ])
+
+        self.add_journal(units="sfc")
+        self.add_journal(units="sfc-state-propagation")
+
+        self.add_cmd_output([
+            "mlnx-sf -a show",                  # short cli output
+            "mlnx-sf -a show --json --pretty",  # verbose json output
         ])
