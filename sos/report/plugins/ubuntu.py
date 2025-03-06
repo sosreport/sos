a65bad64fb2e3629a2f0133564ba7b336d6d0904
Arif Ali <arif.ali@canonical.com>
2025-03-06 18:41:23 +0000

    [ubuntu] Revamp of plugin
    * Use the packages tuple to trigger for plugin
    * Remove the if statement to check for package install, this seemed redundant
    * Add collection of uaclient.conf
    
    Signed-off-by: Arif Ali <arif.ali@canonical.com>

--- sos/report/plugins/ubuntu.py
+++ sos/report/plugins/ubuntu.py
@@ -17,34 +17,43 @@ class Ubuntu(Plugin, UbuntuPlugin):
     plugin_name = 'ubuntu'
     profiles = ('system',)
 
+    packages = (
+        'ubuntu-advantage-tools',
+        'ubuntu-pro-client',
+        'update-manager-core',
+    )
+
     def setup(self):
         self.add_cmd_output([
             "ubuntu-security-status --thirdparty --unavailable",
             "hwe-support-status --verbose",
         ])
 
-        if self.is_installed('ubuntu-advantage-tools'):
-            if is_executable('ua'):
-                ua_tools_status = 'ua status'
-            elif is_executable('pro'):
-                ua_tools_status = 'pro status'
-            else:
-                ua_tools_status = 'ubuntu-advantage status'
-            ua_pred = SoSPredicate(self, kmods=['tls'])
-            self.add_cmd_output(ua_tools_status,
-                                pred=ua_pred, changes=True)
-            self.add_cmd_output(f"{ua_tools_status} --format json",
-                                pred=ua_pred, changes=True)
+        if is_executable('ua'):
+            ua_tools_status = 'ua status'
+        elif is_executable('pro'):
+            ua_tools_status = 'pro status'
+        else:
+            ua_tools_status = 'ubuntu-advantage status'
+        ua_pred = SoSPredicate(self, kmods=['tls'])
+        self.add_cmd_output(ua_tools_status,
+                            pred=ua_pred, changes=True)
+        self.add_cmd_output(f"{ua_tools_status} --format json",
+                            pred=ua_pred, changes=True)
 
-            if not self.get_option("all_logs"):
-                self.add_copy_spec([
-                    "/var/log/ubuntu-advantage.log",
-                    "/var/log/ubuntu-advantage.log.1",
-                    "/var/log/ubuntu-advantage.log.2*",
-                    "/var/log/ubuntu-advantage-timer.log",
-                    "/var/log/ubuntu-advantage-timer.log.1",
-                    "/var/log/ubuntu-advantage-timer.log.2*",
-                ])
-            else:
-                self.add_copy_spec("/var/log/ubuntu-advantage.log*")
-                self.add_copy_spec("/var/log/ubuntu-advantage-timer.log*")
+        self.add_copy_spec("/etc/ubuntu-advantage/uaclient.conf")
+
+        if not self.get_option("all_logs"):
+            self.add_copy_spec([
+               "/var/log/ubuntu-advantage.log",
+               "/var/log/ubuntu-advantage.log.1",
+               "/var/log/ubuntu-advantage.log.2*",
+               "/var/log/ubuntu-advantage-timer.log",
+               "/var/log/ubuntu-advantage-timer.log.1",
+               "/var/log/ubuntu-advantage-timer.log.2*",
+            ])
+        else:
+            self.add_copy_spec("/var/log/ubuntu-advantage.log*")
+            self.add_copy_spec("/var/log/ubuntu-advantage-timer.log*")
+
+# vim: set et ts=4 sw=4 :
