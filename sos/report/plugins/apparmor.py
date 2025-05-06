f7b338e2c9db5126b87cdcd53f3114ccc03aa916
Wesley Hershberger <wesley.hershberger@canonical.com>
2025-05-06 14:45:54 -0500

    [tests] Call tests decorated with <distro>_only
    Decorators require that the returned wrapper function call the function
    that was passed. Without this commit, any test using these decorators
    was not run.
    
    This also resolves a few failures that were masked by this bug:
    - The `ubuntu` plugin should not run for Debian
    - The `apparmor` plugin should run for Debian
    - Removed `openshift` warning message check (not emitted by default
      in ce289a3)
    - `anaconda` is not installed in Fedora 40+ by default
    - `yum` plugin was removed in 8cdc77c4
    - Add version number to `sos report` ui log
    
    Signed-off-by: Wesley Hershberger <wesley.hershberger@canonical.com>

--- sos/report/plugins/apparmor.py
+++ sos/report/plugins/apparmor.py
@@ -8,10 +8,10 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
-from sos.report.plugins import Plugin, UbuntuPlugin
+from sos.report.plugins import Plugin, DebianPlugin
 
 
-class Apparmor(Plugin, UbuntuPlugin):
+class Apparmor(Plugin, DebianPlugin):
 
     short_desc = 'Apparmor mandatory access control'
 
