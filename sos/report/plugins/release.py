a7ba5fd131c6c018409325137f11d117d98a0ebf
Jose Castillo <jcastillo@redhat.com>
2025-07-10 14:15:37 +0100

    [kdump|release] Change format of imports
    Change the format of the import line for plugins
    to parenthesis so it matches what we do in all
    other plugins that contain multiline imports.
    Also, this change ensures that the script
    plugins_overview.py's output is consistent
    across all plugins and doesn't contain
    errors.
    
    Related: #4071
    
    Signed-off-by: Jose Castillo <jcastillo@redhat.com>

--- sos/report/plugins/release.py
+++ sos/report/plugins/release.py
@@ -6,8 +6,8 @@
 #
 # See the LICENSE file in the source distribution for further information.
 
-from sos.report.plugins import Plugin, RedHatPlugin, \
-    DebianPlugin, UbuntuPlugin, CosPlugin
+from sos.report.plugins import (Plugin, RedHatPlugin, DebianPlugin,
+                                UbuntuPlugin, CosPlugin)
 
 
 class Release(Plugin, UbuntuPlugin, CosPlugin):
