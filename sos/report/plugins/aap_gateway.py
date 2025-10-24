db22936b6e2f18e6883621aeffe2cb4f66df5033
Nagoor Shaik <nshaik@redhat.com>
2025-10-24 16:52:05 +0530

    [aap_*] fix passwords and secrets obfuscation
    Signed-off-by: Nagoor Shaik <nshaik@redhat.com>
    
    EDA Version in AAP 2.4 uses environment file to store configuration
    ex: /etc/ansible-automation-platform/eda/environment
    
    EDA version AAP 2.5 and above uses a YAML file to store config
    ex: /etc/ansible-automation-platform/eda/settings.yaml

--- sos/report/plugins/aap_gateway.py
+++ sos/report/plugins/aap_gateway.py
@@ -49,8 +49,8 @@ class AAPGatewayPlugin(Plugin, RedHatPlugin):
 
     def postproc(self):
         # remove database password
-        jreg = r"(DATABASE_PASSWORD)(\s*)(=|:)(\s*)(.*)"
-        repl = r"\1\2\3\4********"
+        jreg = r"(\s*'PASSWORD'\s*:\s*)('.*')"
+        repl = r"\1********"
         self.do_path_regex_sub(
             "/etc/ansible-automation-platform/gateway/settings.py",
             jreg,
