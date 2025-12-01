e749992ecc5aa3ddd08913cad5f4a6301442fc70
Christoph Böhmwalder <christoph.boehmwalder@linbit.com>
2025-12-01 18:45:07 +0100

    [drbd] Modernize and extend plugin
    The current drbd plugin is aimed at older DRBD 8-based setups, and only
    collects some basic data.
    Extend the plugin to collect more valuable data for real-world support
    use cases.
    This version has DRBD 9 in mind, but stays backwards compatible to
    version 8.
    
    Signed-off-by: Christoph Böhmwalder <christoph.boehmwalder@linbit.com>

--- sos/report/plugins/drbd.py
+++ sos/report/plugins/drbd.py
@@ -7,27 +7,74 @@
 # See the LICENSE file in the source distribution for further information.
 
 from sos.report.plugins import Plugin, RedHatPlugin, UbuntuPlugin
+from sos.utilities import find
 
 
-class DRDB(Plugin, RedHatPlugin, UbuntuPlugin):
+class DRBD(Plugin, RedHatPlugin, UbuntuPlugin):
 
     short_desc = 'Distributed Replicated Block Device (DRBD)'
 
     plugin_name = 'drbd'
     profiles = ('storage',)
-    packages = ('drbd*-utils',)
+    commands = ('drbdsetup',)
+
+    # In case of kernel bugs, drbdsetup may hang indefinitely.
+    # Set a shorter than default timeout.
+    cmd_timeout = 60
+
+    def add_drbd_thread_stacks(self):
+        stacks = ""
+        for pid_file in find("*_pid", "/sys/kernel/debug/drbd/resources/"):
+            with open(pid_file, 'r', encoding='utf-8') as f:
+                pid = f.read().strip()
+            stacks += f"--- {pid_file}: {pid} ---\n"
+            if pid.isdigit():
+                try:
+                    sfn = f"/proc/{pid}/stack"
+                    with open(sfn, 'r', encoding='utf-8') as sf:
+                        stacks += sf.read()
+                except Exception as e:
+                    stacks += f"Could not read /proc/{pid}/stack: {e}\n"
+            stacks += "\n"
+
+        self.add_string_as_file(stacks, "drbd_thread_stacks")
 
     def setup(self):
         self.add_cmd_output([
-            "drbd-overview",
-            "drbdadm dump-xml",
-            "drbdsetup status all",
+            "drbdadm dump",
+            "drbdadm -d -vvv adjust all",
+            "drbdsetup status -vs all",
             "drbdsetup show all"
         ])
+        self.add_drbd_thread_stacks()
+        for kmod in find("drbd*.ko*", "/lib/modules"):
+            self.add_cmd_output([
+                f"modinfo {kmod}",
+            ], suggest_filename=f"modinfo_{kmod.replace('/', '_')}")
         self.add_copy_spec([
             "/etc/drbd.conf",
             "/etc/drbd.d/*",
-            "/proc/drbd"
+            "/proc/drbd",
+            "/sys/kernel/debug/drbd/*",
+            "/var/lib/drbd/*",
+            "/var/lib/drbd-support/*",
+            "/var/lib/linstor.d/*"
         ])
+
+    def postproc(self):
+        # Scrub nodehash from /var/lib/drbd-support/registration.json
+        nodehash_re = r'("nodehash"\s*:\s*")[a-zA-Z0-9]+"'
+        repl = r'\1********"'
+        self.do_path_regex_sub(
+            '/var/lib/drbd-support/registration.json',
+            nodehash_re, repl
+        )
+
+        # Scrub shared secret from *.{conf,res} files and command outputs
+        secret_re = r'(shared-secret\s+\"?).[^\"]+(\"?\s*;)'
+        repl = r'\1********\2'
+        self.do_path_regex_sub(r'.*\.(conf|res)', secret_re, repl)
+        self.do_cmd_output_sub("drbdadm dump", secret_re, repl)
+        self.do_cmd_output_sub("drbdsetup show all", secret_re, repl)
 
 # vim: set et ts=4 sw=4 :
