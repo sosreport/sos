1e58e700565b08ef689383e113dc7a9212e19467
Danylo Bereznenko <pepsiqqfanta@gmail.com>
2025-12-19 15:50:01 +0100

    Add postproc with scrubbing passwords Added new exports Slightly updated paths
    Signed-off-by: Danylo Bereznenko <pepsiqqfanta@gmail.com>

--- sos/report/plugins/charmed_opensearch.py
+++ sos/report/plugins/charmed_opensearch.py
@@ -8,9 +8,10 @@
 
 import re
 import os
-from sos.report.plugins import Plugin,PluginOpt, IndependentPlugin
 
-class OpenSearch(Plugin, IndependentPlugin):
+from sos.report.plugins import Plugin,PluginOpt, UbuntuPlugin
+
+class OpenSearch(Plugin, UbuntuPlugin):
     short_desc = 'Charmed OpenSearch'
     plugin_name = 'charmed_opensearch'
     option_list = [
@@ -29,10 +30,13 @@ class OpenSearch(Plugin, IndependentPlugin):
     ]
 
     log_path = "/var/log/opensearch"
-    config_path = "/etc/opensearch/opensearch.yml"
-    user,password = None, None
+    etc_path = "/etc/opensearch"
+    certificates_path = f"{etc_path}/certificates"
+    config_path = f"{etc_path}/opensearch.yml"
+
     snap_current_path = "/var/snap/opensearch/current"
     snap_common_path = "/var/snap/opensearch/common"
+    user,password = None, None
 
     def setup(self):
         self.user, self.password = self.get_credentials()
@@ -40,17 +44,63 @@ class OpenSearch(Plugin, IndependentPlugin):
             self.export_vm()
 
     def check_vm(self):
-        return os.path.exists(self.snap_common_path)
+        return os.path.exists(self.snap_current_path)
 
     def export_vm(self):
-        opensearch_config_file = f"{self.snap_current_path}{self.config_path}"
-        self.add_copy_spec(opensearch_config_file)
+        opensearch_config_directory= f"{self.snap_current_path}{self.etc_path}"
+        # FORBIDDEN
+        self.add_forbidden_path([
+            f"{opensearch_config_directory}/certificates",
+            f"{opensearch_config_directory}/opensearch.keystore",
+        ])
 
+        # CONFIGS
+        self.add_copy_spec(f"{opensearch_config_directory}/*")
+
+        # CERTIFICATES
+        # We don't export certificates, just paths
+        self.add_cmd_output(
+            f"ls -la {self.snap_current_path}{self.certificates_path}",
+            suggest_filename="certificates_paths"
+        )
+
+        # JOURNAL
+        self.add_journal(units="snap.opensearch.*", lines=1000, allfields=True)
+
+        # SERVICES
+        self.add_service_status("snap.opensearch.daemon",
+                                suggest_filename="service_status_opensearch")
+        self.add_cmd_output(
+            "systemctl cat snap.opensearch.daemon.service",
+            suggest_filename="service_cat_opensearch"
+        )
+
+        # LOGS
         if self.get_option("all_log"):
             self.add_copy_spec(f"{self.snap_common_path}{self.log_path}/*")
         else:
             self.add_copy_spec(f"{self.snap_common_path}{self.log_path}/*.log")
-        host, port = self.get_hostname_port(opensearch_config_file)
+
+        # SNAP
+        self.add_cmd_output(
+            "snap get opensearch -d",
+            suggest_filename="snap_get_opensearch"
+        )
+
+        # PORTS
+        self.add_cmd_output(
+            "sh -c \"ss -tulpn | awk 'NR==1 || /9200/'\"",
+            suggest_filename="listening_ports"
+        )
+
+        # PROCESS TREE
+        self.add_cmd_output(
+            "sh -c \"ps aux | awk 'NR==1 || /opensearch/'\"",
+            suggest_filename="process_tree"
+        )
+
+        # API
+        host, port = self.get_hostname_port(f"{self.snap_current_path}{self.config_path}")
         base_url = f"https://{host}:{port}"
         self.export_api(base_url)
 
@@ -88,3 +138,16 @@ class OpenSearch(Plugin, IndependentPlugin):
         self.add_cmd_output(f"{base_cmd} '{base_url}/_cat/indices'", suggest_filename="indices")
         self.add_cmd_output(f"{base_cmd} '{base_url}/_cat/shards'", suggest_filename="shards")
         self.add_cmd_output(f"{base_cmd} '{base_url}/_cat/aliases'", suggest_filename="aliases")
+
+    def postproc(self):
+        # SCRUB PASSWORDS
+        if self.check_vm():
+            opensearch_config_file = f"{self.snap_current_path}{self.config_path}"
+        else:
+            opensearch_config_file = ""
+
+        self.do_path_regex_sub(
+            f"{opensearch_config_file}",
+            r"(\s*plugins\.security\.ssl\..+password\s*:\s+).*",
+            r'\1"*********"',
+        )
