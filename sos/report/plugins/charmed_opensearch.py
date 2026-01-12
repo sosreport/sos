ce189e9b64e08553a9a1677768c02fd142926467
Danylo Bereznenko <pepsiqqfanta@gmail.com>
2026-01-12 16:40:23 +0100

    Remove embedded shell scripting Add password validation And other general fixes
    Signed-off-by: Danylo Bereznenko <pepsiqqfanta@gmail.com>

--- sos/report/plugins/charmed_opensearch.py
+++ sos/report/plugins/charmed_opensearch.py
@@ -7,12 +7,11 @@
 # See the LICENSE file in the source distribution for further information.#
 
 import re
-import os
 
 from sos.report.plugins import Plugin, PluginOpt, UbuntuPlugin
 
 
-class OpenSearch(Plugin, UbuntuPlugin):
+class CharmedOpenSearch(Plugin, UbuntuPlugin):
     short_desc = 'Charmed OpenSearch'
     plugin_name = 'charmed_opensearch'
     option_list = [
@@ -21,12 +20,8 @@ class OpenSearch(Plugin, UbuntuPlugin):
             desc="Username for opensearch, to check APIs"
         ),
         PluginOpt(
-            "pass", default="", val_type=str,
+            "password", default="", val_type=str,
             desc="Password for opensearch, to check APIs",
-        ),
-        PluginOpt(
-            "all_log", default=True, val_type=bool,
-            desc="Export all logs",
         )
     ]
 
@@ -37,17 +32,10 @@ class OpenSearch(Plugin, UbuntuPlugin):
 
     snap_current_path = "/var/snap/opensearch/current"
     snap_common_path = "/var/snap/opensearch/common"
-    user, password = None, None
 
     def setup(self):
-        self.user, self.password = self.get_credentials()
-        if self.check_vm():
-            self.export_vm()
-
-    def check_vm(self):
-        return os.path.exists(self.snap_current_path)
-
-    def export_vm(self):
+        if not self.check_vm():
+            return
         opensearch_config_directory = (f"{self.snap_current_path}"
                                        f"{self.etc_path}")
         # FORBIDDEN
@@ -60,9 +48,8 @@ class OpenSearch(Plugin, UbuntuPlugin):
         self.add_copy_spec(f"{opensearch_config_directory}/*")
 
         # CERTIFICATES
-        # We don't export certificates, just paths
-        self.add_cmd_output(
-            f"ls -la {self.snap_current_path}{self.certificates_path}",
+        self.add_dir_listing(
+            f"{self.snap_current_path}{self.certificates_path}",
             suggest_filename="certificates_paths"
         )
 
@@ -72,13 +59,13 @@ class OpenSearch(Plugin, UbuntuPlugin):
         # SERVICES
         self.add_service_status("snap.opensearch.daemon",
                                 suggest_filename="service_status_opensearch")
-        self.add_cmd_output(
-            "systemctl cat snap.opensearch.daemon.service",
-            suggest_filename="service_cat_opensearch"
+        self.add_copy_spec(
+            "/etc/systemd/system/"
+            "snap.opensearch.daemon.service"
         )
 
         # LOGS
-        if self.get_option("all_log"):
+        if self.get_option("all_logs"):
             self.add_copy_spec(f"{self.snap_common_path}{self.log_path}/*")
         else:
             self.add_copy_spec(f"{self.snap_common_path}{self.log_path}/*.log")
@@ -89,23 +76,41 @@ class OpenSearch(Plugin, UbuntuPlugin):
             suggest_filename="snap_get_opensearch"
         )
 
-        # PORTS
-        self.add_cmd_output(
-            "sh -c \"ss -tulpn | awk 'NR==1 || /9200/'\"",
-            suggest_filename="listening_ports"
-        )
-
-        # PROCESS TREE
-        self.add_cmd_output(
-            "sh -c \"ps aux | awk 'NR==1 || /opensearch/'\"",
-            suggest_filename="process_tree"
-        )
-
         # API
         host, port = self.get_hostname_port(
             f"{self.snap_current_path}{self.config_path}")
         base_url = f"https://{host}:{port}"
         self.export_api(base_url)
+
+    def check_vm(self):
+        return self.path_exists(self.snap_current_path)
+
+    def collect(self):
+        # PORTS
+        with self.collection_file('listening_ports') as pofile:
+            res = self.exec_cmd("ss -tulpn")
+            if not res['status'] == 0:
+                pofile.write(f"Unable to get ports list: {res['output']}")
+                return
+            lines = res['output'].splitlines()
+            filtered_ports = [
+                line for index, line in enumerate(lines)
+                if index == 0 or "9200" in line
+            ]
+            pofile.writelines('\n'.join(filtered_ports) + '\n')
+
+        # PROCESS TREE
+        with self.collection_file('process_tree') as prfile:
+            res = self.exec_cmd("ps aux")
+            if not res['status'] == 0:
+                prfile.write(f"Unable to get process list: {res['output']}")
+                return
+            lines = res['output'].splitlines()
+            filtered_procs = [
+                line for index, line in enumerate(lines)
+                if index == 0 or "opensearch" in line
+            ]
+            prfile.writelines('\n'.join(filtered_procs) + '\n')
 
     def get_hostname_port(self, opensearch_config_file):
         """ Get hostname and port number """
@@ -128,13 +133,10 @@ class OpenSearch(Plugin, UbuntuPlugin):
             self._log_info(f"Failed to parse {opensearch_config_file}: {err}")
         return hostname, port
 
-    def get_credentials(self):
-        user = self.get_option("user")
-        password = self.get_option("pass")
-        return user, password
-
     def export_api(self, base_url):
-        base_cmd = f"curl -k -u {self.user}:{self.password} -X GET"
+        if not (password := self.get_option('password')):
+            return
+        base_cmd = f"curl -k -u {self.get_option('user')}:{password} -X GET"
         self.add_cmd_output(
             f"{base_cmd} '{base_url}/_cluster/settings?pretty'",
             suggest_filename="settings"
@@ -157,11 +159,8 @@ class OpenSearch(Plugin, UbuntuPlugin):
         if self.check_vm():
             opensearch_config_file = (f"{self.snap_current_path}"
                                       f"{self.config_path}")
-        else:
-            opensearch_config_file = ""
-
-        self.do_path_regex_sub(
-            f"{opensearch_config_file}",
-            r"(\s*plugins\.security\.ssl\..+password\s*:\s+).*",
-            r'\1"*********"',
-        )
+            self.do_path_regex_sub(
+                f"{opensearch_config_file}",
+                r"(\s*plugins\.security\.ssl\..+password\s*:\s+).*",
+                r'\1"*********"',
+            )
