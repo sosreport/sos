267ceecc76367ee52b79f9774d7fd047db62c739
Songhee Yeon <song-hee.yeon@navercorp.com>
2026-09-23 11:14:22 +0900

    [policies] Add Navix policy
    Navix is an RPM-based, RHEL-compatible distribution maintained by
    NAVER Cloud. Without a matching policy sos falls back to
    GenericLinuxPolicy, so the RPM package list is not collected and
    RedHatPlugin-only plugins are skipped. This policy inherits
    RedHatPolicy so both work.
    
    Signed-off-by: Songhee Yeon <song-hee.yeon@navercorp.com>

---
+++ sos/policies/distros/navix.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2026 Songhee Yeon <song-hee.yeon@navercorp.com>
+
+# This file is part of the sos project: https://github.com/sosreport/sos
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# version 2 of the GNU General Public License.
+#
+# See the LICENSE file in the source distribution for further information.
+
+from sos.policies.distros.redhat import RedHatPolicy
+
+
+class NavixPolicy(RedHatPolicy):
+    vendor = "NAVER Cloud Corp"
+    vendor_urls = [('Distribution Website', 'https://navix.navercorp.com')]
+    os_release_file = '/etc/navix-release'
+    os_release_name = 'Navix'
+    os_release_id = 'navix'
+
+# vim: set et ts=4 sw=4 :
